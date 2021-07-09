885ddbf7fda174c0ca64c415ff99ec87fc1e1c46
Vasco Santos <vasco.santos@moxy.studio>
2021-07-09 15:29:26 +0200

    feat: delete user upload (#48)
    Adds DELETE /user/uploads/:id to API and wires it up in UI
    
    Given we are going with DELETE /user/uploads/:id I didn't add a delete to the client, as this is not a /car route. Happy to add it as well
    
    All flow tested with local setup from website.

---
+++ packages/db/fauna/resources/Function/deleteUserUpload.js
@@ -0,0 +1,52 @@
+import fauna from 'faunadb'
+
+const {
+  Function,
+  CreateFunction,
+  Query,
+  Lambda,
+  Let,
+  Var,
+  If,
+  Update,
+  Exists,
+  Now,
+  Ref,
+  Abort,
+  Collection,
+  Get,
+  Equals,
+  Select
+} = fauna
+
+const name = 'deleteUserUpload'
+const body = Query(
+  Lambda(
+    ['userId', 'uploadId'],
+    Let(
+      {
+        uploadRef: Ref(Collection('Upload'), Var('uploadId')),
+      },
+      If(
+        // make sure the upload exists
+        Exists(Var('uploadRef')),
+        If(
+          // make sure the user owns this upload
+          Equals(
+            Select(['data', 'user', 'id'], Get(Var('uploadRef'))),
+            Var('userId')
+          ),
+          Update(Var('uploadRef'), { data: { deleted: Now() } }),
+          Abort('unauthorized')
+        ),
+        Abort('not found')
+      )
+    )
+  )
+)
+
+export default If(
+  Exists(Function(name)),
+  Update(Function(name), { name, body }),
+  CreateFunction({ name, body })
+)
