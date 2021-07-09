eb1b30bc0e3705df2a97ffde611ba1d6bc6a8b67
Oli Evans <oli@tableflip.io>
2021-07-09 15:52:32 +0100

    chore: fix website staging deploy (#47)
    - build client before website
    - remove env hardcoding
    - set env in CI workflow

--- packages/website/lib/constants.js
+++ packages/website/lib/constants.js
@@ -1,20 +1,5 @@
-let API = /** @type {string} **/ (process.env.NEXT_PUBLIC_API)
-let MAGIC_TOKEN = /** @type {string} **/ (process.env.NEXT_PUBLIC_MAGIC)
-
-if (globalThis.window) {
-  switch (location.host) {
-    case 'staging.web3.storage':
-      API = 'https://api-staging.web3.storage'
-      MAGIC_TOKEN = ''
-      break
-    case 'web3.storage':
-      API = 'https://api.web3.storage'
-      MAGIC_TOKEN = ''
-      break
-    default:
-      break
-  }
-}
+const API = /** @type {string} **/ (process.env.NEXT_PUBLIC_API)
+const MAGIC_TOKEN = /** @type {string} **/ (process.env.NEXT_PUBLIC_MAGIC)
 
 export default {
   API: API,
