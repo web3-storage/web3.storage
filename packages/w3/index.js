ebc26e5d968bf08f517c80129f0717bcbd6c529f
Oli Evans <oli@tableflip.io>
2021-07-10 21:33:59 +0100

    feat: subcommands for w3
    - `w3 get <cid>` fetch files by cid and verify them
    - `w3 put <path>` upload files to web3.storage
    
    switches `meow` for `sade` for the subcommand support.
    
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- packages/w3/index.js
+++ packages/w3/index.js
@@ -2,22 +2,20 @@ import { globSource } from './utils/glob-source.js'
 import { writeFiles } from 'ipfs-car/unpack/fs'
 import { Web3Storage } from 'web3.storage'
 
-export default async function ({ input, flags }) {
-  const client = new Web3Storage({ token: flags.token, endpoint: flags.api })
-  if (flags.get) {
-    const res = await client.get(flags.get)
-    await writeFiles(res.unixFsIterator(), flags.output)
-    return
+export async function get (cid, opts) {
+  const client = new Web3Storage({ token: opts.token, endpoint: opts.api })
+  const res = await client.get(cid)
+  await writeFiles(res.unixFsIterator(), opts.output)
+}
+
+export async function put (path, opts) {
+  const client = new Web3Storage({ token: opts.token, endpoint: opts.api })
+  const files = []
+  console.log('')
+  for await (const file of globSource(path)) {
+    console.log(`+ ${file.name}`)
+    files.push(file)
   }
-  if (flags.put) {
-    const files = []
-    console.log('')
-    for await (const file of globSource(flags.put)) {
-      console.log(`+ ${file.name}`)
-      files.push(file)
-    }
-    const root = await client.put(files)
-    console.log(`⁂ https://dweb.link/ipfs/${root}`)
-    return
-  }
+  const root = await client.put(files)
+  console.log(`⁂ https://dweb.link/ipfs/${root}`)
 }
