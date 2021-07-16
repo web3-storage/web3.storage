40d852ab1fd6e24e4607aad3d963f340161494fc
Oli Evans <oli@tableflip.io>
2021-07-16 17:39:17 +0100

    feat: `w3 token` % `w3 status` commands
    `w3 token` will prompt you to paste in your api key
    
    ```console
    $ w3 token
    ✔ Paste your API token for api.web3.storage · eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJk...etc
    
    ⁂ API token saved
    ```
    
    and as with all other commands, you can pass in a custom --api
    
    ```console
    w3 token --api https://api-staging.web3.storage
    ✔ Paste your API token for api-staging.web3.storage ·
    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJk...etc
    
    ⁂ API token saved
    ```
    
    and now w3 will save that token and api (via [`conf`](https://github.com/sindresorhus/conf) to an appropriate config dir per you OS. AND! it warns you if you are using an API other than PROD! 🎉
    ```console
    w3 put ~/Pictures/pmf.jpg
    ⁂ using api-staging.web3.storage
    ⁂ Stored 1 file
    ⁂ https://dweb.link/ipfs/bafybeicwehe4cx74f5gf2duol7g5kgqmkh4z7ivnqikgqgspckqazz6ygi
    ```
    
    as a bonus, the `w3 status <cid>` command is also ready to print some JSON about what we know of IPFS pins and Filecoin deals for a given CID, pending #95 landing.
    
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- packages/w3/bin.js
+++ packages/w3/bin.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import sade from 'sade'
-import { get, put } from './index.js'
+import { get, put, status, token } from './index.js'
 
 const cli = sade('w3')
 
@@ -10,6 +10,17 @@ cli
   .option('--token', 'API token to use')
   .example('put path/to/files')
   .example('get bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy -o room-guardian.jpg')
+
+cli.command('token')
+  .option('--api', 'URL for API to use')
+  .option('--token', 'API token to use')
+  .describe('Save an API token use for all requests')
+  .action(token)
+
+cli.command('status <cid>')
+  .describe('Get the Filecoin deals and IPFS pins that contain a given CID, as JSON.')
+  .example('status bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy')
+  .action(status)
 
 cli.command('get <cid>')
   .describe('Fetch and verify files from web3.storage')
