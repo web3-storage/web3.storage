001dae1375bfdbbee9e72d95e94065fb87ead11b
Oli Evans <oli@tableflip.io>
2021-07-19 11:40:05 +0100

    feat: `w3 token` and `w3 status` commands (#98)
    `w3 token` will prompt you to paste in your api key
    
    ```console
    $ w3 token
    ✔ Paste your API token for api.web3.storage · eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJk...etc
    
    ⁂ API token saved
    ```
    
    and as with all other commands, you can pass in a custom --api
    
    ```console
    w3 token --api https://api-staging.web3.storage
    ✔ Paste your API token for api-staging.web3.storage · eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJk...etc
    
    ⁂ API token saved
    ```
    
    and now w3 will save that token and api (via [`conf`](https://github.com/sindresorhus/conf) to an appropriate config dir per you OS. AND! it warns you if you are using an API other than PROD! 🎉
    ```console
    w3 put ~/Pictures/pmf.jpg
    ⁂ using api-staging.web3.storage
    ⁂ Stored 1 file
    ⁂ https://dweb.link/ipfs/bafybeicwehe4cx74f5gf2duol7g5kgqmkh4z7ivnqikgqgspckqazz6ygi
    ```
    
    as a bonus, the `w3 status <cid>` command is also ready to print some JSON about what we know of IPFS pins and Filecoin deals for a given CID, pending #95 landing.
    
    
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- packages/w3/bin.js
+++ packages/w3/bin.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 
 import sade from 'sade'
-import { get, put } from './index.js'
+import { get, put, status, token } from './index.js'
 
 const cli = sade('w3')
 
 cli
-  .option('--api', 'URL for API to use')
-  .option('--token', 'API token to use')
   .example('put path/to/files')
   .example('get bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy -o room-guardian.jpg')
+
+cli.command('token')
+  .option('--api', 'URL for the Web3 Storage API. Default: https://api.web3.storage')
+  .option('--delete', 'Delete your saved token')
+  .describe('Save an API token to use for all requests.')
+  .action(token)
+
+cli.command('put <path>')
+  .describe('Upload a file or directory to web3.storage')
+  .action(put)
 
 cli.command('get <cid>')
   .describe('Fetch and verify files from web3.storage')
@@ -17,8 +25,9 @@ cli.command('get <cid>')
   .example('get bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy -o room-guardian.jpg')
   .action(get)
 
-cli.command('put <path>')
-  .describe('Upload a file or directory to web3.storage')
-  .action(put)
+cli.command('status <cid>')
+  .describe('Get the Filecoin deals and IPFS pins that contain a given CID, as JSON.')
+  .example('status bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy')
+  .action(status)
 
 cli.parse(process.argv)
