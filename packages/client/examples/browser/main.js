5609981a6341eb2fb218eacda9b44d6eba81239e
Vasco Santos <vasco.santos@moxy.studio>
2021-07-19 12:23:44 +0200

    chore: use files-from-path in Node and file picker in the browser

--- packages/client/examples/browser/main.js
+++ packages/client/examples/browser/main.js
@@ -1,47 +1,19 @@
-/* global File */
 import { Web3Storage } from 'web3.storage'
 
 const endpoint = 'https://api.web3.storage' // the default
 const token =
   new URLSearchParams(window.location.search).get('key') || 'API_KEY' // your API key from https://web3.storage/manage
 
-async function main () {
-  const storage = new Web3Storage({ endpoint, token })
+const storage = new Web3Storage({ endpoint, token })
 
-  const files = prepareFiles()
+document.getElementById("filepicker").addEventListener("change", async function (event) {
+  console.log('Using endpoint:', endpoint)
 
-  // send the files to web3.storage
+  let cidP = document.getElementById("cid")
+  let files = event.target.files
+
   const cid = await storage.put(files)
 
-  // TODO
-  console.log('added', cid)
-  // check that the CID is pinned
-  // const status = await store.status(cid)
-  // log(status)
-}
-
-function prepareFiles () {
-  const data = 'Hello web3.storage!'
-  const data2 = 'Hello web3.storage!!'
-
-  return [
-    new File(
-      [data],
-      '/dir/data.txt'
-    ),
-    new File(
-      [data2],
-      '/dir/data2.txt'
-    ),
-    new File(
-      [data],
-      '/dir/otherdir/data.txt'
-    ),
-    new File(
-      [data2],
-      '/dir/otherdir/data2.txt'
-    )
-  ]
-}
-
-main()
+  const text = document.createTextNode(`Content added with CID: ${cid}`)
+  cidP.appendChild(text)
+}, false)
--- packages/client/examples/node.js/put-files.js
+++ packages/client/examples/node.js/put-files.js
@@ -1,56 +1,28 @@
-import fs from 'fs'
 import process from 'process'
-import { Web3Storage, File } from '../../src/lib.js'
+import minimist from 'minimist'
+import { getFilesFromPath } from 'files-from-path'
+import { Web3Storage } from 'web3.storage'
 
-const endpoint = process.env.ENDPOINT || 'https://api.web3.storage' // the default
-const token = process.env.TOKEN // your API key from https://web3.storage/manage
-
-// TODO: Receive as cli param
+const defaultEndpoint = 'https://api.web3.storage' // the default
 
 async function main () {
+  const args = minimist(process.argv.slice(2))
+  const token = args['token']
+  const endpoint = args['endpoint'] || defaultEndpoint
+
   if (!token) {
     console.error('A token is needed. You can create one on https://web3.storage/manage')
     return
   }
 
-  console.log('endpoint', endpoint)
-  console.log('token', token)
+  console.log('Using endpoint:', endpoint)
 
   const storage = new Web3Storage({ endpoint, token })
 
-  // const files = prepareFiles()
+  const files = await getFilesFromPath('./fixtures')
+  const cid = await storage.put(files)
 
-  const cid = await storage.put([{
-    stream: () => fs.createReadStream('./fixtures/a.txt'),
-    name: 'fixtures/a.txt'
-  }])
-
-  console.log('added', cid)
-}
-
-// TODO: Read a fixtures folder instead
-function prepareFiles () {
-  const data = 'Hello web3.storage!!!'
-  const data2 = 'Hello web3.storage!!!!'
-
-  return [
-    new File(
-      [data],
-      '/dir/data.txt'
-    ),
-    new File(
-      [data2],
-      '/dir/data2.txt'
-    ),
-    new File(
-      [data],
-      '/dir/otherdir/data.txt'
-    ),
-    new File(
-      [data2],
-      '/dir/otherdir/data2.txt'
-    )
-  ]
+  console.log('Content added with CID:', cid)
 }
 
 main()
