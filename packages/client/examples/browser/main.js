f1b2fd47b3a0542b12ae2247a78cdba445ee5c12
Vasco Santos <vasco.santos@moxy.studio>
2021-07-20 09:45:42 +0200

    chore: remove endpoint arg and add dependency version

--- packages/client/examples/browser/main.js
+++ packages/client/examples/browser/main.js
@@ -1,14 +1,12 @@
 import { Web3Storage } from 'web3.storage'
 
-const endpoint = 'https://api.web3.storage' // the default
+const endpoint = 'https://api.web3.storage'
 const token =
   new URLSearchParams(window.location.search).get('key') || 'API_KEY' // your API key from https://web3.storage/manage
 
 const storage = new Web3Storage({ endpoint, token })
 
 document.getElementById('filepicker').addEventListener('change', async function (event) {
-  console.log('Using endpoint:', endpoint)
-
   const cidP = document.getElementById('cid')
   const files = event.target.files
 
--- packages/client/examples/node.js/put-files.js
+++ packages/client/examples/node.js/put-files.js
@@ -3,19 +3,16 @@ import minimist from 'minimist'
 import { getFilesFromPath } from 'files-from-path'
 import { Web3Storage } from 'web3.storage'
 
-const defaultEndpoint = 'https://api.web3.storage' // the default
+const endpoint = 'https://api.web3.storage'
 
 async function main () {
   const args = minimist(process.argv.slice(2))
   const token = args.token
-  const endpoint = args.endpoint || defaultEndpoint
 
   if (!token) {
     console.error('A token is needed. You can create one on https://web3.storage')
     return
   }
-
-  console.log('Using endpoint:', endpoint)
 
   const storage = new Web3Storage({ endpoint, token })
 
