1e75fc95c85984c68b0cd2b90a1b4139ae7179cc
Vasco Santos <vasco.santos@moxy.studio>
2021-07-21 17:32:05 +0200

    feat: POST /upload (#110)

--- packages/api/src/constants.js
+++ packages/api/src/constants.js
@@ -1,3 +1,5 @@
 export const GATEWAY = new URL('https://ipfs.io')
 export const JWT_ISSUER = 'web3-storage'
 export const METRICS_CACHE_MAX_AGE = 10 * 60 // in seconds (10 minutes)
+export const LOCAL_ADD_THRESHOLD = 1024 * 1024 * 2.5
+export const DAG_SIZE_CALC_LIMIT = 1024 * 1024 * 9
