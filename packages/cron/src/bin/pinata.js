96e6e69c3b5f9179e4b10489e81c443cb966103d
Oli Evans <oli@tableflip.io>
2021-07-21 21:59:16 +0100

    feat: adds pinata cron job
    Fetch the oldest 600 PinRequests from the DB and pin them on Piñata
    
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

---
+++ packages/cron/src/bin/pinata.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import dotenv from 'dotenv'
+import { pinToPinata } from '../jobs/pinata.js'
+import { getPinata, getDBClient } from '../lib/utils.js'
+
+async function main () {
+  const env = process.env.ENV || 'dev'
+  const db = getDBClient(process.env)
+  const pinata = getPinata(process.env, { reqsPerSec: 2 })
+  await pinToPinata({ db, pinata, env })
+}
+
+dotenv.config()
+main()
