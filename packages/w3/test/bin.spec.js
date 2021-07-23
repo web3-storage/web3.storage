a16863338d32cefd3db57c227ac88b39e1e18b4b
Oli Evans <oli@tableflip.io>
2021-07-23 13:38:52 +0100

    chore: add tests
    and check that no deps are missing by testing in ci
    
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

---
+++ packages/w3/test/bin.spec.js
@@ -0,0 +1,36 @@
+/* eslint-env mocha */
+import execa from 'execa'
+import assert from 'assert'
+
+describe('w3', () => {
+  it('can be executed', async () => {
+    try {
+      execa.sync('./bin.js')
+      assert.fail('Should exit with error code')
+    } catch (err) {
+      assert.match(err.stderr, /No command specified./)
+    }
+  })
+})
+
+describe('w3 put', () => {
+  it('offers help', () => {
+    try {
+      execa.sync('./bin.js', ['put'])
+      assert.fail('Should exit with error code')
+    } catch (err) {
+      assert.match(err.stderr, /Insufficient arguments!/)
+    }
+  })
+})
+
+describe('w3 get', () => {
+  it('offers help', () => {
+    try {
+      execa.sync('./bin.js', ['get'])
+      assert.fail('Should exit with error code')
+    } catch (err) {
+      assert.match(err.stderr, /Insufficient arguments!/)
+    }
+  })
+})
