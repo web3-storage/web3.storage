25a5135cb746f429d8b842db32bf2805345d491b
Oli Evans <oli@tableflip.io>
2021-07-23 19:04:07 +0100

    Apply suggestions from code review
    Co-authored-by: Alan Shaw <alan.shaw@protocol.ai>

--- packages/client/src/lib/interface.ts
+++ packages/client/src/lib/interface.ts
@@ -76,15 +76,18 @@ export type PutOptions = {
    */
   maxRetries?: number
   /**
-   * Should input files be wrapped with a directory. 
+   * Should input files be wrapped with a directory?
    * Enabling it preserves the input filenames in DAG as they become
    * directory entries in the generated wrapping dir. 
    * 
    * The trade off is your root CID will be that of the wrapping dir, 
    * rather than the input file itself. 
    * 
-   * so for a single file. e.g. `cat.png` its ipfs path would be 
+   * For a single file e.g. `cat.png` it's IPFS path would be 
    * `<wrapping dir cid>/cat.png` rather than just `<cid for cat.png>`
+   *
+   * Wrapping with a directory is required when passing multiple files
+   * that do not share the same root.
    */
   wrapWithDirectory?: boolean
   /**
