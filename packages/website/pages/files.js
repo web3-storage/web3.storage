c0abdb318c6b5e039c77a6689450c16a25bfb348
Rafael Ramalho <rafazelramalho19@gmail.com>
2021-07-27 18:20:26 +0100

    feat: add refresh button

--- packages/website/pages/files.js
+++ packages/website/pages/files.js
@@ -182,12 +182,11 @@ export default function Files({ user }) {
   const queryParams = { before: befores[0], size }
   /** @type {[string, { before: string, size: number }]} */
   const queryKey = ['get-uploads', queryParams]
-  const { isLoading, isFetching, data } = useQuery(
+  const { isLoading, isFetching, data, refetch } = useQuery(
     queryKey,
     (ctx) => getUploads(ctx.queryKey[1]),
     {
       enabled: !!user,
-      refetchInterval: /* Every minute */ 60 * 1000 
     }
   )
 
@@ -321,7 +320,11 @@ export default function Files({ user }) {
                     {/* <Button small className="ml-2">
                       Export Deals
                     </Button> */}
+                    
                     <div className="w-35 ml-auto">
+                      <Button small onClick={() => refetch()}>Refresh</Button>
+                    </div>
+                    <div className="w-35 ml-4">
                       <Button href="/upload" small id="upload">Upload More Files</Button>
                     </div>
                   </div>
