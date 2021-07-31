0f8e2db47b205c28f55cfd13a65d3cdd378a8b3c
Alan Shaw <alan.shaw@protocol.ai>
2021-07-31 13:25:19 +0100

    fix: social card

--- packages/website/components/layout.js
+++ packages/website/components/layout.js
@@ -44,11 +44,7 @@ export default function Layout({
         <meta property="og:description" content={description} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://web3.storage" />
-        <meta
-          property="og:image"
-          content="https://web3.storage/images/social-card.png"
-        />
-
+        <meta property="og:image" content="https://web3.storage/social-card.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@protocollabs" />
         <meta name="twitter:creator" content="@protocollabs" />
