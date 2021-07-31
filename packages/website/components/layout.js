df3d0ca2811ac98f1786852bc7b23d31cabf8934
Alan Shaw <alan.shaw@protocol.ai>
2021-07-31 13:52:09 +0100

    fix: more social image

--- packages/website/components/layout.js
+++ packages/website/components/layout.js
@@ -40,12 +40,14 @@ export default function Layout({
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta property="image" content="https://web3.storage/social-card.png" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://web3.storage" />
         <meta property="og:image" content="https://web3.storage/social-card.png" />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="https://docs.web3.storage/images/social-card.png" />
         <meta name="twitter:site" content="@protocollabs" />
         <meta name="twitter:creator" content="@protocollabs" />
       </Head>
