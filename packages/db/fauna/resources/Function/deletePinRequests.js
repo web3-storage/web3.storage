191ad1a78a3f02f4633d8783206251754dec272d
Oli Evans <oli@tableflip.io>
2021-08-24 11:45:27 +0100

    fix: delete fauna history when deleting PinRequest
    Suggested by the fauna team, a similar fix to https://github.com/web3-storage/web3.storage/pull/385
    
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

---
+++ packages/db/fauna/resources/Function/deletePinRequests.js
@@ -0,0 +1,33 @@
+import fauna from 'faunadb'
+
+const {
+  Function,
+  CreateFunction,
+  Query,
+  Lambda,
+  Var,
+  If,
+  Update,
+  Exists,
+  Map,
+  Collection,
+  Ref,
+  Call
+} = fauna
+
+const name = 'deletePinRequests'
+const body = Query(
+  Lambda(
+    ['requests'],
+    Map(
+      Var('requests'),
+      Lambda('id', Call('deleteWithHistory', Ref(Collection('PinRequest'), Var('id'))))
+    )
+  )
+)
+
+export default If(
+  Exists(Function(name)),
+  Update(Function(name), { name, body }),
+  CreateFunction({ name, body })
+)
