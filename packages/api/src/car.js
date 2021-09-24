29a538988db9472ab1b7d6aa001ae90f31768579
Vasco Santos <vasco.santos@moxy.studio>
2021-09-24 11:31:27 +0200

    chore: rebase

--- packages/api/src/car.js
+++ packages/api/src/car.js
@@ -380,18 +380,6 @@ function cumulativeSize (pbNodeBytes, pbNode) {
 }
 
 /**
- * The sum of the node size and size of each link
- * @param {import('@ipld/dag-pb/src/interface').PBNode} pbNode
- * @returns {number} the size of the DAG in bytes
- */
-function cumulativeSize (pbNodeBytes, pbNode) {
-  // NOTE: Tsize is optional, but all ipfs implementations we know of set it.
-  // It's metadata, that could be missing or deliberately set to an incorrect value.
-  // This logic is the same as used by go/js-ipfs to display the cumulative size of a dag-pb dag.
-  return pbNodeBytes.length + pbNode.Links.reduce((acc, curr) => acc + (curr.Tsize || 0), 0)
-}
-
-/**
  * @param {import('@nftstorage/ipfs-cluster').StatusResponse['peerMap']} peerMap
  */
 function toPins (peerMap) {
