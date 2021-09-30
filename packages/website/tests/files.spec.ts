963341a4a0a6da3c106e5fd43384ee3e0cc72b66
Rafael Ramalho <rafazelramalho19@gmail.com>
2021-09-30 15:44:25 +0100

    ci: add ci variables

--- packages/website/tests/files.spec.ts
+++ packages/website/tests/files.spec.ts
@@ -3,7 +3,6 @@ import { authenticate } from './utils/auth'
 
 test.describe('Uploading Files', () => {
   test.beforeEach(async ({ page, baseURL }) => {
-    console.log(baseURL)
     await authenticate(page, baseURL)
     await page.goto("/upload");
   })
