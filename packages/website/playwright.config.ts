13ce92298257b68c1331de6ff7a11861eb40d62a
Rafael Ramalho <rafazelramalho19@gmail.com>
2021-09-30 15:21:23 +0100

    ci: add node_env ci

--- packages/website/playwright.config.ts
+++ packages/website/playwright.config.ts
@@ -2,7 +2,6 @@ const config = {
   globalSetup: require.resolve('./tests/global-setup'),
   use: {
     baseURL: process.env.E2E_BASE_URL || 'https://web3.storage',
-    // storageState: './tests/state.json',
     headless: process.env.NODE_ENV !== 'development',
   },
 };
--- packages/website/tests/global-setup.ts
+++ packages/website/tests/global-setup.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import path from 'path'
 
-dotenv.config({ path: path.resolve(process.cwd(), '.env.local') })
+process.env.NODE_ENV !== 'ci' && dotenv.config({ path: path.resolve(process.cwd(), '.env.local') })
 
 // FIXME: Issue with magic auth + playwright, tracking on https://github.com/microsoft/playwright/issues/9112
 async function globalSetup(/* config */) {}
