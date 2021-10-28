c9cd8642f9c9d21d98b3d9e9061d8549b82f35ef
Rafael Ramalho <rafazelramalho19@gmail.com>
2021-10-28 20:46:50 +0100

    feat: add migration indexes (#573)
    Co-authored-by: Vasco Santos <santos.vasco10@gmail.com>

---
+++ packages/db/fauna/resources/Migration_indexes/migration_upload.js
@@ -0,0 +1,31 @@
+import fauna from 'faunadb'
+
+const {
+  CreateIndex,
+  Collection,
+  If,
+  Index,
+  Exists,
+  Not
+} = fauna
+
+/**
+ * Usage:
+ *
+ * Match(Index('upload_migration_timestamp'))
+ */
+const index = {
+  name: 'upload_migration_timestamp',
+  source: Collection('Upload'),
+  values: [
+    { field: ['data', 'updated'] },
+    { field: 'ref' }
+  ]
+}
+
+// indexes cannot be updated
+export default If(
+  Not(Exists(Index(index.name))),
+  CreateIndex(index),
+  Index(index.name)
+)
