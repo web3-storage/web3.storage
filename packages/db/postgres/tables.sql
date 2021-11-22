33b37d6444c3b44b9fc71746d6785bc1f40efa58
Paolo <paolo@potatolondon.com>
2021-11-22 12:56:32 +0000

    feat(pinning-api-db): update documentation

--- packages/db/postgres/tables.sql
+++ packages/db/postgres/tables.sql
@@ -206,7 +206,7 @@ CREATE TABLE IF NOT EXISTS pa_pin_request
  id              BIGSERIAL PRIMARY KEY,
  -- Points to the pinned content, it is updated once the content is actually being found.
  content_cid     TEXT                                                         REFERENCES content (cid),
-  -- Points to the pinned content, it is updated once the content is actually being found.
+  -- Points to auth key used to pin the content.
  auth_key_id         BIGINT                                                       NOT NULL REFERENCES public.user (id),
  -- The id of the content being requested, it could not exist on IPFS (typo, node offline etc)
  requested_cid   TEXT NOT NULL,
