0e233cc5af499a032cf83e3454eefbfb7ef2b0a0
Vasco Santos <santos.vasco10@gmail.com>
2021-11-23 17:46:47 +0000

    fix: deal normalize should filter out unexpected deal status (#682)

--- packages/api/test/mocks/pgrest/post_rpc#find_deals_by_content_cids.js
+++ packages/api/test/mocks/pgrest/post_rpc#find_deals_by_content_cids.js
@@ -5,17 +5,30 @@
 module.exports = ({ body }) => {
   // main cid
   if (body.cids.includes('bafybeifnfkzjeohjf2dch2iqqpef3bfjylwxlcjws2msvdfyze5bvdprfm')) {
-    return [{
-      dealId: 12345,
-      storageProvider: 'f99',
-      status: 'Active',
-      pieceCid: 'baga',
-      dataCid: 'bafybeifnfkzjeohjf2dch2iqqpef3bfjylwxlcjws2msvdfyze5bvdprfm',
-      dataModelSelector: 'Links/0/Links',
-      activation: '<iso timestamp>',
-      created: '2021-07-14T19:27:14.934572Z',
-      updated: '2021-07-14T19:27:14.934572Z'
-    }]
+    return [
+      {
+        dealId: 12345,
+        storageProvider: 'f99',
+        status: 'Active',
+        pieceCid: 'baga',
+        dataCid: 'bafybeifnfkzjeohjf2dch2iqqpef3bfjylwxlcjws2msvdfyze5bvdprfm',
+        dataModelSelector: 'Links/0/Links',
+        dealActivation: '<iso timestamp>',
+        created: '2021-07-14T19:27:14.934572Z',
+        updated: '2021-07-14T19:27:14.934572Z'
+      },
+      {
+        dealId: 123456,
+        storageProvider: 'f98',
+        status: 'Terminated',
+        pieceCid: 'baga',
+        dataCid: 'bafybeifnfkzjeohjf2dch2iqqpef3bfjylwxlcjws2msvdfyze5bvdprfm',
+        dataModelSelector: 'Links/0/Links',
+        dealActivation: '<iso timestamp>',
+        created: '2021-07-14T19:27:14.934572Z',
+        updated: '2021-07-14T19:27:14.934572Z'
+      }
+    ]
   } else if (body.cids.includes('bafybeica6klnrhlrbx6z24icefykpbwyypouglnypvnwb5esdm6yzcie3q')) {
     return [{
       status: 'Queued',
--- packages/db/utils.js
+++ packages/db/utils.js
@@ -35,19 +35,45 @@ export function normalizeContent (content) {
  * @return {Array<import('../db-client-types').PinItemOutput>}
  */
 export function normalizePins (pins) {
-  return pins.map(pin => ({
-    _id: pin._id,
-    status: pin.status,
-    created: pin.created,
-    updated: pin.updated,
-    peerId: pin.location.peerId,
-    peerName: pin.location.peerName,
-    region: pin.location.region
-  })).filter(pin => PIN_STATUS.has(pin.status))
+  return pins.filter(pin => PIN_STATUS.has(pin.status))
+    .map(pin => ({
+      _id: pin._id,
+      status: pin.status,
+      created: pin.created,
+      updated: pin.updated,
+      peerId: pin.location.peerId,
+      peerName: pin.location.peerName,
+      region: pin.location.region
+    }))
+}
+
+/**
+ * Normalize deal items.
+ */
+export function normalizeDeals (deals) {
+  return deals.filter(deal => DEAL_STATUS.has(deal.status))
+    .map(deal => ({
+      dealId: deal.dealId,
+      storageProvider: deal.storageProvider,
+      status: deal.status,
+      pieceCid: deal.pieceCid,
+      dataCid: deal.dataCid,
+      dataModelSelector: deal.dataModelSelector,
+      activation: deal.dealActivation,
+      expiration: deal.dealExpiration,
+      created: deal.created,
+      updated: deal.updated
+    }))
 }
 
 const PIN_STATUS = new Set([
   'Pinned',
   'Pinning',
   'PinQueued'
+])
+
+const DEAL_STATUS = new Set([
+  'Queued',
+  'Published',
+  'Active'
 ])
