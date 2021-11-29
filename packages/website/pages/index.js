2d390817eb72df7a6b41d527f440a7580a900373
Ben Munoz <benmunozj@gmail.com>
2021-11-29 12:04:11 -0500

    chore Removed unnecessary default props

--- packages/website/pages/index.js
+++ packages/website/pages/index.js
@@ -3,20 +3,6 @@ import IndexPageData from '../content/pages/index.json';
 import Navigation from '../components/navigation/navigation.js';
 import BlockBuilder from '../components/blockbuilder/blockbuilder.js';
 // import countly from 'Lib/countly'
-// ====================================================================== Params
-/**
- * @returns {{ props: import('../components/types.js').LayoutProps}}
- */
-export function getStaticProps() {
-  return {
-    props: {
-      needsLoggedIn: false,
-      navBgColor: 'bg-w3storage-red',
-      footerBgColor: 'bg-w3storage-red',
-      highlightMessage: `Looking to store NFTs? Check out <a class="underline" href="https://nft.storage">NFT.Storage</a>!</p>`,
-    },
-  };
-}
 // ===================================================================== Exports
 export default function Home() {
   const sections = IndexPageData.page_content;
