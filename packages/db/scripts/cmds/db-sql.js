5bd1ad3f96049139af932ac762254e04dc7326e0
Vasco Santos <santos.vasco10@gmail.com>
2021-12-02 16:35:43 +0100

    chore: address review

--- packages/db/scripts/cmds/db-sql.js
+++ packages/db/scripts/cmds/db-sql.js
@@ -14,23 +14,18 @@ const { Client } = pg
 /**
  * @param {Object} [opts]
  * @param {boolean} [opts.reset]
+ * @param {boolean} [opts.cargo]
  * @param {boolean} [opts.testing]
  */
-export async function dbSqlCmd ({ reset, testing } = {}) {
+export async function dbSqlCmd ({ reset, cargo, testing } = {}) {
   // read all the SQL files
-  const configSql = fs.readFileSync(path.join(__dirname, '../../postgres/config.sql'), {
-    encoding: 'utf-8'
-  })
+  const configSql = fs.readFileSync(path.join(__dirname, '../../postgres/config.sql'), 'utf-8')
 
-  const tablesSql = fs.readFileSync(path.join(__dirname, '../../postgres/tables.sql'), {
-    encoding: 'utf-8'
-  })
-  const functionsSql = fs.readFileSync(path.join(__dirname, '../../postgres/functions.sql'), {
-    encoding: 'utf-8'
-  })
-  const resetSql = fs.readFileSync(path.join(__dirname, '../../postgres/reset.sql'), {
-    encoding: 'utf-8'
-  })
+  const tablesSql = fs.readFileSync(path.join(__dirname, '../../postgres/tables.sql'), 'utf-8')
+  const functionsSql = fs.readFileSync(path.join(__dirname, '../../postgres/functions.sql'), 'utf-8')
+  const resetSql = fs.readFileSync(path.join(__dirname, '../../postgres/reset.sql'), 'utf-8')
+  const cargoSql = fs.readFileSync(path.join(__dirname, '../../postgres/cargo.sql'), 'utf-8')
+  let fdwSql = fs.readFileSync(path.join(__dirname, '../../postgres/fdw.sql'), 'utf-8')
 
   // Setup postgres client
   const connectionString = process.env.PG_CONNECTION
@@ -52,6 +47,19 @@ export async function dbSqlCmd ({ reset, testing } = {}) {
 
   if (testing) {
     await client.query(cargoSchema)
+  } else if (cargo) {
+    // Replace secrets in the FDW sql file
+    fdwSql = fdwSql.replace(":'DAG_CARGO_HOST'", `'${process.env.DAG_CARGO_HOST}'`)
+    fdwSql = fdwSql.replace(":'DAG_CARGO_DATABASE'", `'${process.env.DAG_CARGO_DATABASE}'`)
+    fdwSql = fdwSql.replaceAll(":'DAG_CARGO_USER'", `'${process.env.DAG_CARGO_USER}'`)
+    fdwSql = fdwSql.replaceAll(":'DAG_CARGO_PASSWORD'", `'${process.env.DAG_CARGO_PASSWORD}'`)
+
+    // Create web3_storage role
+    await client.query(`
+CREATE ROLE web3_storage LOGIN;
+    `)
+    await client.query(fdwSql)
+    await client.query(cargoSql)
   }
 
   await client.query(functionsSql)
