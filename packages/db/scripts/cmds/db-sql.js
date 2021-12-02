42c6500bcc3683da5f51bcbe99c9f45190571e99
Vasco Santos <santos.vasco10@gmail.com>
2021-12-02 14:50:35 +0100

    fix: not need cargo credentials for testing

--- packages/db/scripts/cmds/db-sql.js
+++ packages/db/scripts/cmds/db-sql.js
@@ -32,19 +32,6 @@ export async function dbSqlCmd ({ reset, cargo, testing } = {}) {
   const resetSql = fs.readFileSync(path.join(__dirname, '../../postgres/reset.sql'), {
     encoding: 'utf-8'
   })
-  let cargoSql = fs.readFileSync(path.join(__dirname, '../../postgres/cargo.sql'), {
-    encoding: 'utf-8'
-  })
-
-  let fdwSql = fs.readFileSync(path.join(__dirname, '../../postgres/fdw.sql'), {
-    encoding: 'utf-8'
-  })
-
-  // Replace secrets in the FDW sql file
-  fdwSql = fdwSql.replace(":'DAG_CARGO_HOST'", `'${process.env.DAG_CARGO_HOST}'`)
-  fdwSql = fdwSql.replace(":'DAG_CARGO_DATABASE'", `'${process.env.DAG_CARGO_DATABASE}'`)
-  fdwSql = fdwSql.replace(":'DAG_CARGO_USER'", `'${process.env.DAG_CARGO_USER}'`)
-  fdwSql = fdwSql.replace(":'DAG_CARGO_PASSWORD'", `'${process.env.DAG_CARGO_PASSWORD}'`)
 
   // Setup postgres client
   const connectionString = process.env.PG_CONNECTION
@@ -65,28 +52,45 @@ export async function dbSqlCmd ({ reset, cargo, testing } = {}) {
   await client.query(tablesSql)
 
   if (cargo) {
-    if (testing) {
-      cargoSql = cargoSql.replace(
-        `
--- Create materialized view from cargo "aggregate_entries" table
-CREATE MATERIALIZED VIEW public.aggregate_entry
-AS
-SELECT *
-FROM cargo.aggregate_entries;`,
-        `
-CREATE MATERIALIZED VIEW public.aggregate_entry
-AS
-SELECT *
-FROM cargo.aggregate_entries 
-WHERE cid_v1 in ('bafybeiaj5yqocsg5cxsuhtvclnh4ulmrgsmnfbhbrfxrc3u2kkh35mts4e');
-`
-      )
-    }
-
-    await client.query(fdwSql)
-    await client.query(cargoSql)
+    await client.query(cargoSchema)
   }
 
   await client.query(functionsSql)
   await client.end()
 }
+
+const cargoSchema = `
+CREATE SCHEMA IF NOT EXISTS cargo;
+
+CREATE TABLE IF NOT EXISTS cargo.aggregate_entries (
+  aggregate_cid TEXT NOT NULL,
+  cid_v1 TEXT NOT NULL,
+  datamodel_selector TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cargo.aggregates (
+  aggregate_cid TEXT NOT NULL UNIQUE,
+  piece_cid TEXT UNIQUE NOT NULL,
+  sha256hex TEXT NOT NULL,
+  payload_size BIGINT NOT NULL,
+  metadata JSONB,
+  entry_created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS cargo.deals (
+  deal_id BIGINT UNIQUE NOT NULL,
+  aggregate_cid TEXT NOT NULL,
+  client TEXT NOT NULL,
+  provider TEXT NOT NULL,
+  status TEXT NOT NULL,
+  status_meta TEXT,
+  start_epoch INTEGER NOT NULL,
+  start_time TIMESTAMP WITH TIME ZONE NOT NULL,
+  end_epoch INTEGER NOT NULL,
+  end_time TIMESTAMP WITH TIME ZONE NOT NULL,
+  sector_start_epoch INTEGER,
+  sector_start_time TIMESTAMP WITH TIME ZONE,
+  entry_created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  entry_last_updated TIMESTAMP WITH TIME ZONE NOT NULL
+);
+`
