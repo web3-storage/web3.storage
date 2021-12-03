43dae551e262950d29f8c785d4f58ac2bf8b5b5a
MichaelPhan <michael.d.phan@gmail.com>
2021-12-02 21:21:46 -0500

    fix: Fixing frontend merge issues (#746)
    * fix: Fixing background being disabled
    
    * fix: Fixing dupe button and refs and packages
    
    Co-authored-by: Michael Phan <michael@agencyundone.com>

--- packages/website/pages/_app.tsx
+++ packages/website/pages/_app.tsx
@@ -1,17 +1,21 @@
 import '../styles/global.scss';
 import { Provider } from 'react-redux';
+import { useRouter } from 'next/router';
 
 import withAuthorization from '../store/withAuthorization';
 import withReduxStore from '../store/store';
-// import CorkscrewBackground from '../assets/illustrations/corkscrewBlurred';
+import CorkscrewBackground from '../assets/illustrations/corkscrewBlurred';
 
 /**
  * App root Component
  */
 
-const App = ({ Component, store, pageProps }: any) => {
+const App = ({ Component, store, pageProps, isBase }: any) => {
+  const { pathname } = useRouter();
+
   return (
     <Provider store={store}>
+      {pathname !== '/' && <CorkscrewBackground />}
       <Component {...pageProps} />
     </Provider>
   );
