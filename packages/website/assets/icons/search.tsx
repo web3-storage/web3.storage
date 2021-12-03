0ffebd8baea1e4b7661e72902ca2a9c53909dba1
Michael Phan <michael@agencyundone.com>
2021-12-02 22:37:45 -0500

    feat: wip files manager stylng

---
+++ packages/website/assets/icons/search.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const SearchIcon = props => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={12.207} height={12.207} viewBox="0 0 12.207 12.207" {...props}>
+    <g fill="none" stroke="currentColor">
+      <g>
+        <circle cx={5} cy={5} r={5} stroke="none" />
+        <circle cx={5} cy={5} r={4.5} />
+      </g>
+      <path strokeLinecap="round" d="m8.5 8.5 3 3" />
+    </g>
+  </svg>
+);
+
+export default SearchIcon;
--- packages/website/components/account/filesManager/filesManager.tsx
+++ packages/website/components/account/filesManager/filesManager.tsx
@@ -1,11 +1,31 @@
 import clsx from 'clsx';
 
+import SearchIcon from 'assets/icons/search';
+
 type FilesManagerProps = {
   className?: string;
 };
 
 const FilesManager = ({ className }: FilesManagerProps) => {
-  return <div className={clsx('section files-manager-container', className)}>TODO: Files Manager Content</div>;
+  return (
+    <div className={clsx('section files-manager-container', className)}>
+      <div className="files-manager-header">
+        Files
+        <div className="files-manager-search">
+          <SearchIcon />
+          <input placeholder="Search for a file" />
+        </div>
+      </div>
+      <div className="files-manager-row files-manager-table-header">
+        <span className="file-date">Date</span>
+        <span className="file-name">Name</span>
+        <span className="file-cid">CID</span>
+        <span className="file-pin-status">Pin Status</span>
+        <span className="file-storage-providers">Storage Providers</span>
+        <span className="file-size">Size</span>
+      </div>
+    </div>
+  );
 };
 
 export default FilesManager;
