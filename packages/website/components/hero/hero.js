9c9390b8eb08feaf6eb8a7e7fac446ed2e9be176
Ben Munoz <benmunozj@gmail.com>
2021-12-03 13:16:48 -0500

    feat Added about page to pages directory + page content & styling

--- packages/website/components/hero/hero.js
+++ packages/website/components/hero/hero.js
@@ -8,6 +8,7 @@ import Helix from '../../assets/illustrations/helix';
 import Zigzag from '../../assets/illustrations/zigzag';
 import Cross from '../../assets/illustrations/cross';
 import Triangle from '../../assets/illustrations/triangle';
+import Cluster from '../../assets/illustrations/cluster';
 
 // ====================================================================== Params
 /**
@@ -15,18 +16,40 @@ import Triangle from '../../assets/illustrations/triangle';
  */
 // ====================================================================== Export
 export default function Hero({ block }) {
+  const page = block.page || ''
+  // ================================================================= Functions
+  const getPageHeroHeader = (string) => {
+    switch (string) {
+      case 'index' :
+        return indexHero()
+      case 'pricing' :
+        return (<></>);
+      case 'about' :
+        return (<Cluster id="about_hero-cluster" />);
+    }
+  }
+
+  const indexHero = () => {
+    return (
+      <>
+        <GradientBackground id="index_hero_background-gradient" />
+        <Grid3D id="index_hero-grid-3d" />
+        <Squiggle id="index_hero-squiggle" className={"hero-illustration"} />
+        <Corkscrew id="index_hero-corkscrew" className={"hero-illustration"} />
+        <Zigzag id="index_hero-zigzag" className={"hero-illustration"} />
+        <Helix id="index_hero-helix" className={"hero-illustration"} />
+        <Cross id="index_hero-cross" className={"hero-illustration"} />
+        <Triangle id="index_hero-triangle" className={"hero-illustration"} />
+      </>
+    )
+  }
+
+  // ==================================================== Template [Hero Header]
   return (
-    <div id="index_hero-container">
-      <div className="index_hero-top-section">
-        <div className="index_hero-artwork-container">
-          <GradientBackground id="index_hero_background-gradient" />
-          <Grid3D id="index_hero-grid-3d" />
-          <Squiggle id="index_hero-squiggle" className={"hero-illustration"} />
-          <Corkscrew id="index_hero-corkscrew" className={"hero-illustration"} />
-          <Zigzag id="index_hero-zigzag" className={"hero-illustration"} />
-          <Helix id="index_hero-helix" className={"hero-illustration"} />
-          <Cross id="index_hero-cross" className={"hero-illustration"} />
-          <Triangle id="index_hero-triangle" className={"hero-illustration"} />
+    <div id={`${page}_hero-container`}>
+      <div className={`${page}_hero-top-section`}>
+        <div className={`${page}_hero-artwork-container`}>
+          { getPageHeroHeader(page) }
         </div>
 
         <TextBlock block={block} />
