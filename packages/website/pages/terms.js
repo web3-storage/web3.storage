bc82819b7937fd1e82b15f84d7baa7f8b20b8453
Ben Munoz <benmunozj@gmail.com>
2021-12-08 14:40:48 -0500

    feat Terms and service page added with corresponding markdown file in content

---
+++ packages/website/pages/terms.js
@@ -0,0 +1,35 @@
+// ===================================================================== Imports
+import Markdown from 'markdown-to-jsx';
+
+import Navigation from '../components/navigation/navigation.js';
+import Footer from '../components/footer/footer.js';
+import Helix from '../assets/illustrations/helix';
+import Triangle from '../assets/illustrations/triangle';
+import Cross from '../assets/illustrations/cross';
+import Coil from '../assets/illustrations/coil';
+import Ring from '../assets/illustrations/ring';
+import termsOfService from '../content/terms-of-service.md';
+// ===================================================================== Exports
+export default function Home() {
+  return (
+    <>
+      <Navigation />
+      <main className="page page-terms">
+        <div className="grid">
+          <div className="col-10" data-push-left="off-1">
+            <section id="terms_page-main-body" className="sectional">
+              <Helix id={'terms_page_helix'} />
+              <Triangle id={'terms_page_triangle'} />
+              <Cross id={'terms_page_cross'} />
+              <Ring id={'terms_page_ring'} />
+              <Coil id={'terms_page_coil'} />
+
+              <Markdown>{termsOfService}</Markdown>
+            </section>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
