d06968cc699d0184a3bcbd0871eb6b1d8af222ca
Vasco Santos <santos.vasco10@gmail.com>
2021-12-09 11:49:09 +0100

    fix: create cargo testing

--- packages/db/scripts/cmds/db-sql.js
+++ packages/db/scripts/cmds/db-sql.js
@@ -25,6 +25,7 @@ export async function dbSqlCmd ({ reset, cargo, testing } = {}) {
   const functionsSql = fs.readFileSync(path.join(__dirname, '../../postgres/functions.sql'), 'utf-8')
   const resetSql = fs.readFileSync(path.join(__dirname, '../../postgres/reset.sql'), 'utf-8')
   const cargoSql = fs.readFileSync(path.join(__dirname, '../../postgres/cargo.sql'), 'utf-8')
+  const cargoTesting = fs.readFileSync(path.join(__dirname, '../../postgres/cargo.testing.sql'), 'utf-8')
   let fdwSql = fs.readFileSync(path.join(__dirname, '../../postgres/fdw.sql'), 'utf-8')
 
   // Setup postgres client
@@ -47,7 +48,7 @@ export async function dbSqlCmd ({ reset, cargo, testing } = {}) {
 
   // if testing or cargo fdw flag not set, you just get the schema, no fdw connection to dagcargo
   if (testing && cargo) {
-    await client.query(cargoSchema)
+    await client.query(cargoTesting)
   } else if (!testing && cargo) {
     // Replace secrets in the FDW sql file
     fdwSql = fdwSql.replace(":'DAG_CARGO_HOST'", `'${process.env.DAG_CARGO_HOST}'`)
@@ -62,39 +63,3 @@ export async function dbSqlCmd ({ reset, cargo, testing } = {}) {
   await client.query(functionsSql)
   await client.end()
 }
-
-const cargoSchema = `
-CREATE SCHEMA IF NOT EXISTS cargo;
-
-CREATE TABLE IF NOT EXISTS cargo.aggregate_entries (
-  aggregate_cid TEXT NOT NULL,
-  cid_v1 TEXT NOT NULL,
-  datamodel_selector TEXT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS cargo.aggregates (
-  aggregate_cid TEXT NOT NULL UNIQUE,
-  piece_cid TEXT UNIQUE NOT NULL,
-  sha256hex TEXT NOT NULL,
-  export_size BIGINT NOT NULL,
-  metadata JSONB,
-  entry_created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE IF NOT EXISTS cargo.deals (
-  deal_id BIGINT UNIQUE NOT NULL,
-  aggregate_cid TEXT NOT NULL,
-  client TEXT NOT NULL,
-  provider TEXT NOT NULL,
-  status TEXT NOT NULL,
-  status_meta TEXT,
-  start_epoch INTEGER NOT NULL,
-  start_time TIMESTAMP WITH TIME ZONE NOT NULL,
-  end_epoch INTEGER NOT NULL,
-  end_time TIMESTAMP WITH TIME ZONE NOT NULL,
-  sector_start_epoch INTEGER,
-  sector_start_time TIMESTAMP WITH TIME ZONE,
-  entry_created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  entry_last_updated TIMESTAMP WITH TIME ZONE NOT NULL
-);
-`
