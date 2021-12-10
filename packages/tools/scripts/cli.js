aa3f157526b0b977919608dd89736180306dc348
Paolo <paolo@potatolondon.com>
2021-12-10 10:56:41 +0000

    chore: action PR feedback

--- packages/tools/scripts/cli.js
+++ packages/tools/scripts/cli.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import sade from 'sade'
 import { fileURLToPath } from 'url'
 import execa from 'execa'
+import net from 'net'
 
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -37,6 +38,9 @@ prog
     }
 
     if (start) {
+      if (await isPortReachable(9094)) {
+        throw new Error('Cluster is already running. Please check if you have any docker project or cluster deamon already running.')
+      }
       await execa('docker-compose', [
         '--file',
         composePath,
@@ -72,5 +76,43 @@ prog
     }
 
 }
- 
+
 prog.parse(process.argv)
+
+/**
+ * @param {number} port
+ */
+ export default async function isPortReachable (
+  port,
+  { host = 'localhost', timeout = 1000 } = {}
+) {
+  if (typeof host !== 'string') {
+    throw new TypeError('Specify a `host`')
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    const socket = new net.Socket()
+
+    const onError = (err) => {
+      socket.destroy()
+      reject(err)
+    }
+
+    socket.setTimeout(timeout)
+    socket.once('error', onError)
+    socket.once('timeout', onError)
+
+    socket.connect(port, host, () => {
+      socket.end()
+      resolve(undefined)
+    })
+  })
+
+  try {
+    await promise
+    return true
+  } catch {
+    return false
+  }
+}
+
