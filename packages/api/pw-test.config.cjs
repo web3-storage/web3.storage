17c135d1bcacac4db9da2e87773fad201740ebf5
Vasco Santos <santos.vasco10@gmail.com>
2021-12-10 14:57:03 +0100

    chore: use docker cluster for api tests

--- packages/api/pw-test.config.cjs
+++ packages/api/pw-test.config.cjs
@@ -6,6 +6,7 @@ dotenv.config({
   path: path.join(__dirname, '.env.local')
 })
 
+const toolsCli = path.join(__dirname, '../tools/scripts/cli.js')
 const dbCli = path.join(__dirname, '../db/scripts/cli.js')
 const initScript = path.join(__dirname, './test/fixtures/init-data.sql')
 
@@ -13,18 +14,24 @@ module.exports = {
   // Setup needed docker containers before running tests
   beforeTests: async () => {
     console.log('⚡️ Starting local postgrest container')
-
-    const project = `web3-storage-db-${Date.now()}`
-    await execa(dbCli, ['db', '--start', '--project', project])
+    const projectDb = `web3-storage-db-${Date.now()}`
+    await execa(dbCli, ['db', '--start', '--project', projectDb])
     await execa(dbCli, ['db-sql', '--cargo', '--testing', `--customSqlPath=${initScript}`])
-
     console.log('⚡️ local postgrest container started.')
-    return { project }
+
+    console.log('⚡️ Starting local ipfs-cluster container')
+    const projectCluster = `web3-storage-cluster-${Date.now()}`
+    await execa(toolsCli, ['cluster', '--start', '--project', projectCluster])
+    console.log('⚡️ local ipfs-cluster container started.')
+
+    return { projectDb, projectCluster }
   },
   // Tear down docker containers
   afterTests: async (ctx, beforeTests) => {
     console.log('⚡️ Stopping local postgrest container.')
-    await execa(dbCli, ['db', '--stop', '--clean', '--project', beforeTests.project])
+    await execa(dbCli, ['db', '--stop', '--clean', '--project', beforeTests.projectDb])
+    console.log('⚡️ Stopping local ipfs-cluster container.')
+    await execa(toolsCli, ['cluster', '--stop', '--clean', '--project', beforeTests.projectCluster])
   },
   buildSWConfig: {
     inject: [
