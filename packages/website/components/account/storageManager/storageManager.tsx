8b574d5603e2f874a308db41658b3e2b23f0a196
Michael Phan <michael@agencyundone.com>
2021-12-15 11:45:30 -0500

    feat: New accessability updates

--- packages/website/components/account/storageManager/storageManager.tsx
+++ packages/website/components/account/storageManager/storageManager.tsx
@@ -4,85 +4,47 @@ import { useCallback, useMemo } from 'react';
 import LockIcon from 'assets/icons/lock';
 import Button, { ButtonVariant } from 'components/button/button';
 
-type StorageManagerProps = {
-  className?: string;
-};
-
+// Tiers available
 enum StorageTiers {
   TIER_1 = '0',
   TIER_2 = '1',
   TIER_3 = '2',
 }
 
+// Raw TB number to be used in calculations
 const terabyte = 1099511627776;
 
-const tier1Width = '6.5rem';
-const tier2Width = '7.1825rem';
-const storageInfo: {
-  [key in StorageTiers]: {
-    usageLabel: string;
-    meterLabel: string;
-    usageLimitInTB: number;
-    maxWidth: string;
-  };
-} = {
-  [StorageTiers.TIER_1]: {
-    usageLabel: '1 TB',
-    meterLabel: '<1 TB',
-    usageLimitInTB: terabyte,
-    maxWidth: tier1Width,
-  },
-  [StorageTiers.TIER_2]: {
-    usageLabel: '10 TB',
-    meterLabel: '1-10 TB',
-    usageLimitInTB: terabyte * 10,
-    maxWidth: tier2Width,
-  },
-  [StorageTiers.TIER_3]: {
-    usageLabel: '10 TB+',
-    meterLabel: '10TB+',
-    usageLimitInTB: terabyte * 20,
-    maxWidth: `calc(100% - (${tier1Width} + ${tier2Width}))`,
-  },
-};
+const StorageManager = ({ className = '' }) => {
+  // TODO: Hook up storage tier & storage used to api
+  const storageTier = StorageTiers.TIER_2; // No tier available?
+  const usedStorage = terabyte * 5; // in bytes
 
-const StorageManager = ({ className }: StorageManagerProps) => {
-  // TODO: Hook up storage tier & storage used to redux state
-  const storageTier = StorageTiers.TIER_3; // No tier available?
-  const usedStorage = terabyte * 20; // in bytes
-
-  const { usageLabel } = useMemo(() => storageInfo[storageTier], [storageTier]);
-
-  // Tiered width calculation so bar fills proportionally to tier's allocation & user access level
-  const calculatedWidth = useMemo(() => {
-    // Determine usage relative to tier
-    let storageLeft = usedStorage;
-
-    const widths: string[] = [];
-    Object.keys(storageInfo)
-      // Filter out anything above the current tier if applicable
-      .filter(key => key <= storageTier)
-      // Mapping leftover results to storageInfo object
-      .map(key => storageInfo[key])
-      // Calculating width based off of storage used relative to each tier
-      .forEach(({ usageLimitInTB, maxWidth }, index, currentArray) => {
-        if (storageLeft > 0) {
-          // Base starting point is the accumalation of all previous tier's usageLimitInTB
-          const baseTBStart =
-            index > 0
-              ? // slicing all items up to the current index and combining usage limits to determine a start
-                currentArray
-                  .slice(0, index)
-                  .reduce((acc, { usageLimitInTB: prevTierLimit }) => (acc += prevTierLimit) && acc, 0)
-              : 0;
-
-          widths.push(`calc(${Math.min((baseTBStart + storageLeft) / usageLimitInTB, 1)} * ${maxWidth})`);
-          storageLeft -= usageLimitInTB;
-        }
-      });
-
-    return `calc(${widths.join(' + ')})`;
-  }, [usedStorage, storageTier]);
+  const { maxSpaceLabel, unlockLabel, usedSpacePercentage } = useMemo<{
+    maxSpaceLabel: string;
+    unlockLabel?: string;
+    usedSpacePercentage: number;
+  }>(
+    () =>
+      // Storage information by tier
+      ({
+        [StorageTiers.TIER_1]: {
+          maxSpaceLabel: '1 TB',
+          unlockLabel: '1-10+ TB',
+          usedSpacePercentage: (usedStorage / terabyte) * 100,
+        },
+        [StorageTiers.TIER_2]: {
+          maxSpaceLabel: '10 TB',
+          unlockLabel: '10+ TB',
+          usedSpacePercentage: (usedStorage / (terabyte * 10)) * 100,
+        },
+        [StorageTiers.TIER_3]: {
+          maxSpaceLabel: `${Math.floor(usedStorage / (terabyte * 10) + 1)}0+ TB`,
+          // every increment of 10 changes the amount of space used
+          usedSpacePercentage: ((usedStorage % (terabyte * 10)) / (terabyte * 10)) * 100,
+        },
+      }[storageTier]),
+    [storageTier, usedStorage]
+  );
 
   const onSearchFiles = useCallback(() => {
     window.alert('Search File');
@@ -97,41 +59,34 @@ const StorageManager = ({ className }: StorageManagerProps) => {
       <div className="storage-manager-space">
         <div className="storage-manager-used">
           {/* Used storage in GB */}
-          Storage: {usedStorage / 1073741824} GB of {usageLabel} used
+          Storage: {usedStorage / terabyte}TB of {maxSpaceLabel} used
         </div>
         <Button onClick={onSearchFiles} variant={ButtonVariant.TEXT}>
           Search my files{'\u00A0\u00A0'}❯
         </Button>
       </div>
-      <div className="storage-manager-meter">
-        {/* Mapping out tiers into labeled sections */}
-        {Object.values(StorageTiers).map((tier, currentIndex, arr) => {
-          const activeTierIndex = arr.indexOf(storageTier);
-          const locked = currentIndex > activeTierIndex;
-          const { meterLabel, maxWidth } = storageInfo[tier];
-          return (
-            <div
-              key={tier}
-              style={{ width: maxWidth }}
-              className={clsx('storage-manager-meter-tier', locked && 'locked')}
-            >
-              <span>{meterLabel}</span>
-              {locked && <LockIcon />}
-            </div>
-          );
-        })}
-
-        <div
-          className="storage-manager-meter-used"
-          style={{
-            width: calculatedWidth,
-          }}
-        />
+      <div className="storage-manager-meter-container">
+        <div className="storage-manager-meter">
+          {/* Mapping out tiers into labeled sections */}
+          <div
+            className="storage-manager-meter-used"
+            style={{
+              width: `${Math.min(usedSpacePercentage, 100)}%`,
+            }}
+          />
+          <span className="storage-manager-meter-label">{maxSpaceLabel}</span>
+        </div>
+        {!!unlockLabel && (
+          <Button onClick={onMoreStorage} variant={ButtonVariant.TEXT}>
+            <LockIcon />
+            {unlockLabel}
+          </Button>
+        )}
       </div>
       <div className="storage-manager-info">
-        Need More?
+        Need more free storage?
         <Button onClick={onMoreStorage} variant={ButtonVariant.TEXT}>
-          Unlock more free storage{'\u00A0\u00A0'}❯
+          Submit a request
         </Button>
       </div>
     </div>
