447051a3b897b18a236563d304ecc22d665c3b59
svvimming <benmunozj@gmail.com>
2022-01-17 16:50:37 -0500

    chore Moved 404 page content to general.json under pages content

--- packages/website/pages/404.js
+++ packages/website/pages/404.js
@@ -1,40 +1,17 @@
 // ===================================================================== Imports
-// import GeneralPageData from '../content/pages/general.json';
+import GeneralPageData from '../content/pages/general.json';
 import Navigation from '../components/navigation/navigation.js';
 import Footer from '../components/footer/footer.js';
 import BlockBuilder from '../components/blockbuilder/blockbuilder.js';
 
 // ===================================================================== Exports
-const block = {
-  id: 'error_hero-header',
-  grid: ['middle', 'noGutter'],
-  columns: [
-    {
-      type: 'hero',
-      page: 'error',
-      cols: {
-        num: 'col-8_md-10_sm-8',
-        push_left: 'off-2_md-1_sm-2',
-      },
-      format: 'header',
-      heading: '404',
-      cta: {
-        url: '/',
-        text: 'GO BACK HOME',
-        theme: 'text-purple',
-        event: '',
-        ui: '',
-        action: '',
-      },
-    },
-  ],
-};
+const blocks = GeneralPageData.error_404.sections;
 
 const Custom404 = () => (
   <main className="page-404">
     <Navigation />
 
-    <BlockBuilder id="error_section-1" subsections={[block]} />
+    <BlockBuilder id="error_section-1" subsections={blocks} />
 
     <Footer />
   </main>
