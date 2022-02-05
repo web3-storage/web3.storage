c4c8336e6da0a4b65699e1eff788790612fe96a7
svvimming <53056774+svvimming@users.noreply.github.com>
2022-02-04 21:47:24 -0500

    feat: Floating elements scroll animation (#940)
    * feat Dynamically load scrollmagic library
    
    * feat Add floater transforms passed on scroll scene progress
    
    * feat Floater function reads animation data from page content jsons
    
    * feat Destroy scrollmagic scenes on page component unmount
    
    * feat Marketing site floater animation + about hero fragmentation animation
    
    * chore Linting errors

--- packages/website/pages/index.js
+++ packages/website/pages/index.js
@@ -1,11 +1,28 @@
 // ===================================================================== Imports
+import { useEffect } from 'react';
+
 import IndexPageData from '../content/pages/index.json';
 import Scroll2Top from '../components/scroll2top/scroll2top.js';
 import BlockBuilder from '../components/blockbuilder/blockbuilder.js';
+import { initFloaterAnimations } from '../lib/floater-animations.js';
 
 // ===================================================================== Exports
 export default function Home() {
   const sections = IndexPageData.page_content;
+  const animations = IndexPageData.floater_animations;
+
+  useEffect(() => {
+    let pageFloaters = {};
+    initFloaterAnimations(animations).then(result => {
+      pageFloaters = result;
+    });
+    return () => {
+      if (pageFloaters.hasOwnProperty('destroy')) {
+        pageFloaters.destroy();
+      }
+    };
+  }, [animations]);
+
   return (
     <>
       <main className="page page-index">
