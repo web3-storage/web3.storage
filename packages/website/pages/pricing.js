0085e0c456631b568789428d1661fd49801d711e
orun <orun@agencyundone.com>
2022-03-01 17:48:54 -0500

    feat: tiers page routing renamed pricing throughout

--- packages/website/pages/pricing.js
+++ packages/website/pages/pricing.js
@@ -1,7 +1,7 @@
 // ===================================================================== Imports
 import { useEffect } from 'react';
 
-import TiersPageData from '../content/pages/tiers.json';
+import TiersPageData from '../content/pages/pricing.json';
 import Scroll2Top from '../components/scroll2top/scroll2top.js';
 import BlockBuilder from '../components/blockbuilder/blockbuilder.js';
 import { initFloaterAnimations } from '../lib/floater-animations.js';
@@ -39,7 +39,7 @@ export default function Home() {
 export function getStaticProps() {
   return {
     props: {
-      title: 'Tiers - Web3 Storage - Simple file storage with IPFS & Filecoin',
+      title: 'Pricing - Web3 Storage - Simple file storage with IPFS & Filecoin',
       description:
         'Web3.Storage is a service that grows with your needs, and offers a significant free tier with no strings attached.',
     },
