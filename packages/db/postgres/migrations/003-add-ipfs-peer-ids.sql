f5849d42b2204a094801f269293e4256b79ca3b8
Alan Shaw <alan.shaw@protocol.ai>
2022-03-10 09:49:32 +0000

    fix: remove the duplicates

--- packages/db/postgres/migrations/003-add-ipfs-peer-ids.sql
+++ packages/db/postgres/migrations/003-add-ipfs-peer-ids.sql
@@ -2,17 +2,6 @@
 -- the IPFS node is down)
 ALTER TABLE pin_location ADD COLUMN ipfs_peer_id TEXT;
 -- swap back cluster peer IDs in peer_id and add IPFS peer IDs to ipfs_peer_id
-UPDATE pin_location SET peer_id='12D3KooWF6uxxqZf4sXpQEbNE4BfbVJWAKWrFSKamxmWm4E9vyzd', ipfs_peer_id='12D3KooWPySxxWQjBgX9Jp6uAHQfVmdq8HG1gVvS1fRawHNSrmqW' WHERE peer_id='12D3KooWPySxxWQjBgX9Jp6uAHQfVmdq8HG1gVvS1fRawHNSrmqW';
-UPDATE pin_location SET peer_id='12D3KooWFe387JFDpgNEVCP5ARut7gRkX7YuJCXMStpkq714ziK6', ipfs_peer_id='12D3KooWR19qPPiZH4khepNjS3CLXiB7AbrbAD4ZcDjN1UjGUNE1' WHERE peer_id='12D3KooWR19qPPiZH4khepNjS3CLXiB7AbrbAD4ZcDjN1UjGUNE1';
-UPDATE pin_location SET peer_id='12D3KooWHdBjmicdXu5X57mQ65oBpfy4p3ca5p31kfJT9FSUFu3P', ipfs_peer_id='12D3KooWKytRAd2ujxhGzaLHKJuje8sVrHXvjGNvHXovpar5KaKQ' WHERE peer_id='12D3KooWKytRAd2ujxhGzaLHKJuje8sVrHXvjGNvHXovpar5KaKQ';
-UPDATE pin_location SET peer_id='12D3KooWJeRQfPbiv5U2RqQ9yK3qijbNxKarKEGLMkGrfXJuZ2Bo', ipfs_peer_id='12D3KooWEDMw7oRqQkdCJbyeqS5mUmWGwTp8JJ2tjCzTkHboF6wK' WHERE peer_id='12D3KooWEDMw7oRqQkdCJbyeqS5mUmWGwTp8JJ2tjCzTkHboF6wK';
-UPDATE pin_location SET peer_id='12D3KooWLWFUri36dmTkki6o9PwfQNwGb2gsHuKD5FdcwzCXYnwc', ipfs_peer_id='12D3KooWNuoVEfVLJvU3jWY2zLYjGUaathsecwT19jhByjnbQvkj' WHERE peer_id='12D3KooWNuoVEfVLJvU3jWY2zLYjGUaathsecwT19jhByjnbQvkj';
-UPDATE pin_location SET peer_id='12D3KooWMbibcXHwkSjgV7VZ8TMfDKi6pZvmi97P83ZwHm9LEsvV', ipfs_peer_id='12D3KooWSnniGsyAF663gvHdqhyfJMCjWJv54cGSzcPiEMAfanvU' WHERE peer_id='12D3KooWSnniGsyAF663gvHdqhyfJMCjWJv54cGSzcPiEMAfanvU';
-UPDATE pin_location SET peer_id='12D3KooWGFuccA83SpGGHb92PtM4LAQenxmCYkbB2G2BSEPa2Reo', ipfs_peer_id='12D3KooWRi18oHN1j8McxS9RMnuibcTwxu6VCTYHyLNH2R14qhTy' WHERE peer_id='12D3KooWRi18oHN1j8McxS9RMnuibcTwxu6VCTYHyLNH2R14qhTy';
-UPDATE pin_location SET peer_id='12D3KooWLf5E9SqSuA8BNmFPkokEayfKpHVyg8zT8qEm4Ruz6gk3', ipfs_peer_id='12D3KooWQYBPcvxFnnWzPGEx6JuBnrbF1FZq4jTahczuG2teEk1m' WHERE peer_id='12D3KooWQYBPcvxFnnWzPGEx6JuBnrbF1FZq4jTahczuG2teEk1m';
-UPDATE pin_location SET peer_id='12D3KooW9yCAQ2edw5N75b7t9qCGYb7uFVzkfAgp4KoyTEJKXenF', ipfs_peer_id='12D3KooWJEfH2MB4RsUoaJPogDPRWbFTi8iehsxsqrQpiJwFNDrP' WHERE peer_id='12D3KooWJEfH2MB4RsUoaJPogDPRWbFTi8iehsxsqrQpiJwFNDrP';
-
-
 -- am6
 UPDATE pin_location SET peer_id='12D3KooWF6uxxqZf4sXpQEbNE4BfbVJWAKWrFSKamxmWm4E9vyzd', ipfs_peer_id='12D3KooWPySxxWQjBgX9Jp6uAHQfVmdq8HG1gVvS1fRawHNSrmqW' WHERE peer_id='12D3KooWPySxxWQjBgX9Jp6uAHQfVmdq8HG1gVvS1fRawHNSrmqW';
 -- sv15
