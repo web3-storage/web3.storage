da28a2ccb4e65c87d6c3626b05bcf61ea85729d5
Alan Shaw <alan.shaw@protocol.ai>
2022-03-10 14:02:37 +0000

    perf: improve pins cron

--- packages/db/postgres/tables.sql
+++ packages/db/postgres/tables.sql
@@ -246,6 +246,7 @@ CREATE TABLE IF NOT EXISTS pin_sync_request
 );
 
 CREATE INDEX IF NOT EXISTS pin_sync_request_pin_id_idx ON pin_sync_request (pin_id);
+CREATE INDEX IF NOT EXISTS pin_sync_request_inserted_at_idx ON pin_sync_request (inserted_at);
 
 -- Setting search_path to public scope for uuid function(s)
 SET search_path TO public;
