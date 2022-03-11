ca876ccb27b0d90d622fd6b9504ecc3154457c23
Adam Alton <adamalton@gmail.com>
2022-03-11 12:34:08 +0000

    Make 'cron' package use the same ENV vars for its DB settings as the other packages

--- packages/cron/src/lib/utils.js
+++ packages/cron/src/lib/utils.js
@@ -22,24 +22,13 @@ export function getCluster (env) {
  * @param {Record<string, string|undefined>} env
  */
 export function getDBClient (env) {
-  let token, endpoint
-  if (env.ENV === 'production') {
-    if (!env.PG_REST_JWT) throw new Error('missing PG_REST_JWT environment var')
-    if (!env.PG_REST_URL) throw new Error('missing PG_REST_URL environment var')
-    token = env.PG_REST_JWT
-    endpoint = env.PG_REST_URL
-  } else if (env.ENV === 'staging') {
-    if (!env.STAGING_PG_REST_JWT) throw new Error('missing STAGING_PG_REST_JWT environment var')
-    if (!env.STAGING_PG_REST_URL) throw new Error('missing STAGING_PG_REST_URL environment var')
-    token = env.STAGING_PG_REST_JWT
-    endpoint = env.STAGING_PG_REST_URL
-  } else if (env.ENV === 'dev') {
-    if (!env.DEV_PG_REST_JWT) throw new Error('missing DEV_PG_REST_JWT environment var')
-    if (!env.DEV_PG_REST_URL) throw new Error('missing DEV_PG_REST_URL environment var')
-    token = env.DEV_PG_REST_JWT
-    endpoint = env.DEV_PG_REST_URL
-  } else {
-    throw new Error(`unsupported environment ${env.ENV}`)
+  const token = env.PG_REST_JWT
+  const endpoint = env.PG_REST_URL
+  if (!token) {
+    throw new Error('missing PG_REST_JWT environment var')
+  }
+  if (!endpoint) {
+    throw new Error('missing PG_REST_URL environment var')
   }
   return new DBClient({ token, endpoint, postgres: true })
 }
