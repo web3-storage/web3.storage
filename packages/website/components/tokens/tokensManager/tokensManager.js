328e1ab2a7b82b38897f917af8667cf4f4a32e82
Joanna Ong <ongjfu@gmail.com>
2022-03-21 21:15:29 -0400

    feat: delete modal for token page

--- packages/website/components/tokens/tokensManager/tokensManager.js
+++ packages/website/components/tokens/tokensManager/tokensManager.js
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router';
 import TokenRowItem from './tokenRowItem';
 import countly from 'lib/countly';
 import Loading from 'components/loading/loading';
-import Button from 'components/button/button';
+import Button, { ButtonVariant } from 'components/button/button';
 import { useTokens } from 'components/contexts/tokensContext';
 import Dropdown from 'ZeroComponents/dropdown/dropdown';
 import Filterable from 'ZeroComponents/filterable/filterable';
@@ -15,6 +15,9 @@ import Sortable from 'ZeroComponents/sortable/sortable';
 import Pagination from 'ZeroComponents/pagination/pagination';
 import SearchIcon from 'assets/icons/search';
 import CheckIcon from 'assets/icons/check';
+import Modal from 'modules/zero/components/modal/modal';
+import GradientBackgroundB from 'assets/illustrations/gradient-background-b';
+import CloseIcon from 'assets/icons/close';
 
 const defaultQueryOrder = 'a-z';
 /**
@@ -32,6 +35,7 @@ const TokensManager = ({ content }) => {
   const { query, replace } = useRouter();
   const queryClient = useQueryClient();
   const queryOrderRef = useRef(query.order);
+  const deleteModalState = useState(false);
 
   const [filteredTokens, setFilteredTokens] = useState([]);
   const [sortedTokens, setSortedTokens] = useState([]);
@@ -65,11 +69,6 @@ const TokensManager = ({ content }) => {
 
   const deleteTokenCallback = useCallback(
     async id => {
-      if (!window.confirm('Are you sure? Deleted tokens cannot be recovered!')) {
-        return;
-      }
-      setDeletingTokenId(id);
-
       try {
         await deleteToken(id);
       } finally {
@@ -82,9 +81,22 @@ const TokensManager = ({ content }) => {
         await getTokens();
         setDeletingTokenId('');
       }
+      deleteModalState[1](false);
     },
-    [queryClient, deleteToken, setDeletingTokenId, getTokens]
+    [deleteModalState, deleteToken, queryClient, getTokens]
   );
+
+  const onDeleteSingle = useCallback(
+    async id => {
+      deleteModalState[1](true);
+      setDeletingTokenId(id);
+    },
+    [deleteModalState]
+  );
+
+  const closeDeleteModal = useCallback(() => {
+    deleteModalState[1](false);
+  }, [deleteModalState]);
 
   const showCheckOverlayHandler = useCallback(() => {
     setShowCheckOverlay(true);
@@ -94,81 +106,104 @@ const TokensManager = ({ content }) => {
   }, [setShowCheckOverlay]);
 
   return (
-    <div className="section tokens-manager-container">
-      <div className="tokens-manager-header">
-        <h4>{content.heading}</h4>
-        <Filterable
-          items={tokens}
-          icon={<SearchIcon />}
-          filterKeys={['name', 'secret']}
-          placeholder={content.ui.filter.placeholder}
-          queryParam="filter"
-          onChange={setFilteredTokens}
-        />
-        <Sortable
-          items={filteredTokens}
-          staticLabel={content.ui.sortby.label}
-          options={content.ui.sortby.options}
-          value={defaultQueryOrder}
-          queryParam="order"
-          onChange={setSortedTokens}
-          onSelectChange={showCheckOverlayHandler}
-        />
-      </div>
-      <TokenRowItem name={tokenRowLabels.name.label} secret={tokenRowLabels.secret.label} isHeader />
-      <div className="tokens-manager-table-content">
-        {isFetchingTokens || !fetchDate ? (
-          <Loading className={'tokens-manager-loading-spinner'} />
-        ) : !tokens.length ? (
-          <span className="tokens-manager-upload-cta">
-            {content.table.message}
-            {'\u00A0'}
-            <Button
-              className={clsx(isCreating && 'isDisabled')}
-              href={content.table.cta.link}
-              variant={content.table.cta.theme}
-              tracking={{ ui: countly.ui[content.table.cta.ui], action: content.table.cta.action }}
-            >
-              <Link href={content.table.cta.link}>{content.table.cta.text}</Link>
-            </Button>
-          </span>
-        ) : (
-          paginatedTokens.map(({ name, secret, _id }) => (
-            <TokenRowItem
-              key={secret}
-              id={_id}
-              name={name}
-              secret={secret}
-              deletingTokenId={deletingTokenId}
-              onTokenDelete={() => deleteTokenCallback(_id)}
-            />
-          ))
-        )}
-      </div>
-      <div className="tokens-manager-footer">
-        <Pagination
-          items={sortedTokens}
-          itemsPerPage={itemsPerPage || 10}
-          visiblePages={2}
-          queryParam="page"
-          onChange={setPaginatedTokens}
-          scrollTarget={'.tokens-manager-container'}
-        />
-        <Dropdown
-          className="tokens-manager-result-dropdown"
-          value={content.ui.results.options[0].value}
-          options={content.ui.results.options}
-          queryParam="items"
-          onChange={value => setItemsPerPage(value)}
-          onSelectChange={showCheckOverlayHandler}
-        />
-      </div>
-      <div className={clsx('files-manager-overlay', showCheckOverlay ? 'show' : '')}>
-        <div className="files-manager-overlay-check">
-          <CheckIcon></CheckIcon>
+    <>
+      <div className="section tokens-manager-container">
+        <div className="tokens-manager-header">
+          <h4>{content.heading}</h4>
+          <Filterable
+            items={tokens}
+            icon={<SearchIcon />}
+            filterKeys={['name', 'secret']}
+            placeholder={content.ui.filter.placeholder}
+            queryParam="filter"
+            onChange={setFilteredTokens}
+          />
+          <Sortable
+            items={filteredTokens}
+            staticLabel={content.ui.sortby.label}
+            options={content.ui.sortby.options}
+            value={defaultQueryOrder}
+            queryParam="order"
+            onChange={setSortedTokens}
+            onSelectChange={showCheckOverlayHandler}
+          />
+        </div>
+        <TokenRowItem name={tokenRowLabels.name.label} secret={tokenRowLabels.secret.label} isHeader />
+        <div className="tokens-manager-table-content">
+          {isFetchingTokens || !fetchDate ? (
+            <Loading className={'tokens-manager-loading-spinner'} />
+          ) : !tokens.length ? (
+            <span className="tokens-manager-upload-cta">
+              {content.table.message}
+              {'\u00A0'}
+              <Button
+                className={clsx(isCreating && 'isDisabled')}
+                href={content.table.cta.link}
+                variant={content.table.cta.theme}
+                tracking={{ ui: countly.ui[content.table.cta.ui], action: content.table.cta.action }}
+              >
+                <Link href={content.table.cta.link}>{content.table.cta.text}</Link>
+              </Button>
+            </span>
+          ) : (
+            paginatedTokens.map(({ name, secret, _id }) => (
+              <TokenRowItem
+                key={secret}
+                id={_id}
+                name={name}
+                secret={secret}
+                deletingTokenId={deletingTokenId}
+                onTokenDelete={() => onDeleteSingle(_id)}
+              />
+            ))
+          )}
+        </div>
+        <div className="tokens-manager-footer">
+          <Pagination
+            items={sortedTokens}
+            itemsPerPage={itemsPerPage || 10}
+            visiblePages={2}
+            queryParam="page"
+            onChange={setPaginatedTokens}
+            scrollTarget={'.tokens-manager-container'}
+          />
+          <Dropdown
+            className="tokens-manager-result-dropdown"
+            value={content.ui.results.options[0].value}
+            options={content.ui.results.options}
+            queryParam="items"
+            onChange={value => setItemsPerPage(value)}
+            onSelectChange={showCheckOverlayHandler}
+          />
+        </div>
+        <div className={clsx('files-manager-overlay', showCheckOverlay ? 'show' : '')}>
+          <div className="files-manager-overlay-check">
+            <CheckIcon></CheckIcon>
+          </div>
         </div>
       </div>
-    </div>
+      <Modal
+        className="delete-modal"
+        animation="ken"
+        modalState={deleteModalState}
+        closeIcon={<CloseIcon className="file-uploader-close" />}
+        showCloseButton
+      >
+        <GradientBackgroundB className="account-gradient-background" />
+        <div className="delete-modal-content">
+          <h5>{content?.ui.delete.heading}</h5>
+          <p>{content?.ui.delete.alert}</p>
+        </div>
+        <div className="delete-modal-buttons">
+          <Button variant={ButtonVariant.OUTLINE_DARK} onClick={deleteTokenCallback}>
+            {content?.ui.delete.ok}
+          </Button>
+          <Button variant={ButtonVariant.OUTLINE_DARK} onClick={closeDeleteModal}>
+            {content?.ui.delete.cancel}
+          </Button>
+        </div>
+      </Modal>
+    </>
   );
 };
 
