860873573018ea1ae6020d2ebfbc444138372ece
Joanna Ong <ongjfu@gmail.com>
2022-03-29 13:52:37 -0400

    fix: website 404 (#1172)
    * fix: website 404
    
    * fix: website 404
    
    * fix: syntax typo on 404 docs
    
    * fix: countly on docs
    
    * fix: docs 404

--- packages/website/scripts/docs.sh
+++ packages/website/scripts/docs.sh
@@ -14,3 +14,7 @@ npm run build -w packages/docs
 ## Move Docusaurus site to `packages/website/out/docs`, which is accessible using the Next Zone
 echo -e "Moving Docusaurus site...\n"
 cp -vfr packages/docs/build/* packages/website/out/docs
+
+# rename 404.html/index.html to 404.html since cloudflare pages needs it to be 404.html
+# remove 404.html specific to docs so it reads the main one
+rm -rf packages/website/out/404.html && rm packages/website/out/docs/404.html && cp packages/website/out/404/index.html packages/website/out/404.html
