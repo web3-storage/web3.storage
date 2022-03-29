18152900bc0aca3f2b72878f1860af6a88c2b595
Joanna Ong <ongjfu@gmail.com>
2022-03-29 10:36:44 -0400

    fix: docs 404

--- packages/website/scripts/docs.sh
+++ packages/website/scripts/docs.sh
@@ -16,5 +16,5 @@ echo -e "Moving Docusaurus site...\n"
 cp -vfr packages/docs/build/* packages/website/out/docs
 
 # rename 404.html/index.html to 404.html since cloudflare pages needs it to be 404.html
-# this script is not doc related
-rm -rf packages/website/out/404.html && cp packages/website/out/404/index.html packages/website/out/404.html
+# remove 404.html specific to docs so it reads the main one
+rm -rf packages/website/out/404.html && rm packages/website/out/docs/404.html && cp packages/website/out/404/index.html packages/website/out/404.html
