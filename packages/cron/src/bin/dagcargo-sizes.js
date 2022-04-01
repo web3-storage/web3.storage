8c7840877d1f8e26a58f13e444773824e7e0e7ef
Paolo <paolo@potatolondon.com>
2022-04-01 17:16:46 +0100

    Update script and actions

--- packages/cron/src/bin/dagcargo-sizes.js
+++ packages/cron/src/bin/dagcargo-sizes.js
@@ -1,23 +1,15 @@
 #!/usr/bin/env node
-
 import { updateDagSizes } from '../jobs/dagcargo.js'
 import { envConfig } from '../lib/env.js'
-import { getDBClient, getPg } from '../lib/utils.js'
+import { getDBClient } from '../lib/utils.js'
 
 const oneMonthAgo = () => new Date().setMonth(new Date().getMonth() - 1)
 
 async function main () {
-  const rwPg = getPg(process.env, 'rw')
-  const roPg = getPg(process.env, 'ro')
   const dbClient = getDBClient(process.env)
 
-  try {
-    const after = new Date(process.env.AFTER || oneMonthAgo())
-    await updateDagSizes({ dbClient, after })
-  } finally {
-    await rwPg.end()
-    await roPg.end()
-  }
+  const after = new Date(process.env.AFTER || oneMonthAgo())
+  await updateDagSizes({ dbClient, after })
 }
 
 envConfig()
