3a287b0fa3035c422fb59cabd0acad5942c184f7
Joanna Ong <ongjfu@gmail.com>
2022-04-14 12:09:55 -0400

    fix: build errors

--- packages/website/scripts/postbuild.sh
+++ packages/website/scripts/postbuild.sh
@@ -2,7 +2,3 @@
 
 ## rename 404.html/index.html to 404.html since cloudflare pages needs it to be 404.html
 rm -rf out/404.html && cp out/404/index.html out/404.html
-
-## clean up deploydate that was set in prebuild
-sed -i '' '/^NEXT_PUBLIC_DEPLOYDATE/d' .env 
-sed -i '' '/^$/d' .env 
