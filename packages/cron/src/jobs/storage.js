e39b0e3117ae2cd4b8b41552c43e7a744d0742ac
Gary Homewood <gary@potatolondon.com>
2022-04-19 12:03:33 +0100

    chore: refactor with emails as config const

--- packages/cron/src/jobs/storage.js
+++ packages/cron/src/jobs/storage.js
@@ -3,7 +3,35 @@ import { EmailService } from '../lib/email.js'
 import { EMAIL_TYPE } from '@web3-storage/db'
 
 const log = debug('storage:checkStorageUsed')
-let emailService
+
+const STORAGE_QUOTA_EMAILS = [
+  {
+    emailType: EMAIL_TYPE.User100PercentStorage,
+    fromPercent: 100,
+    secondsSinceLastSent: 60 * 60 * 23
+  },
+  {
+    emailType: EMAIL_TYPE.User90PercentStorage,
+    fromPercent: 90,
+    toPercent: 100,
+    secondsSinceLastSent: 60 * 60 * 23
+  },
+  {
+    emailType: EMAIL_TYPE.User85PercentStorage,
+    fromPercent: 85,
+    toPercent: 90
+  },
+  {
+    emailType: EMAIL_TYPE.User80PercentStorage,
+    fromPercent: 80,
+    toPercent: 85
+  },
+  {
+    emailType: EMAIL_TYPE.User75PercentStorage,
+    fromPercent: 75,
+    toPercent: 80
+  }
+]
 
 /**
  * Get users with storage quota usage in percentage range and email them as
@@ -19,69 +47,29 @@ export async function checkStorageUsed ({ db }) {
 
   log('🗄 Checking users storage quotas')
 
-  emailService = new EmailService({ db })
+  const emailService = new EmailService({ db })
 
-  // Send email every day
-  const usersStorageExceeded = await db.getUsersByStorageUsed({
-    fromPercent: 100
-  })
-  if (usersStorageExceeded.length) {
-    await emailService.sendAdminEmail({
-      users: usersStorageExceeded,
-      email: EMAIL_TYPE.AdminStorageExceeded,
-      secondsSinceLastSent: 60 * 60 * 23
+  for (const email of STORAGE_QUOTA_EMAILS) {
+    const users = await db.getUsersByStorageUsed({
+      fromPercent: email.fromPercent,
+      ...email.toPercent && { toPercent: email.toPercent }
     })
-    await emailService.sendUserEmails({
-      users: usersStorageExceeded,
-      email: EMAIL_TYPE.User100PercentStorage,
-      secondsSinceLastSent: 60 * 60 * 23
-    })
-  }
 
-  const usersFrom90to100 = await db.getUsersByStorageUsed({
-    fromPercent: 90,
-    toPercent: 100
-  })
-  if (usersFrom90to100.length) {
-    await emailService.sendUserEmails({
-      users: usersFrom90to100,
-      email: EMAIL_TYPE.User90PercentStorage,
-      secondsSinceLastSent: 60 * 60 * 23
-    })
-  }
+    if (users.length) {
+      if (email.emailType === EMAIL_TYPE.User100PercentStorage) {
+        await emailService.sendAdminEmail({
+          users,
+          email: EMAIL_TYPE.AdminStorageExceeded,
+          secondsSinceLastSent: 60 * 60 * 23
+        })
+      }
 
-  // Send email every 7 days
-  const usersFrom85to90 = await db.getUsersByStorageUsed({
-    fromPercent: 85,
-    toPercent: 90
-  })
-  if (usersFrom85to90.length) {
-    await emailService.sendUserEmails({
-      users: usersFrom85to90,
-      email: EMAIL_TYPE.User85PercentStorage
-    })
-  }
-
-  const usersFrom80to85 = await db.getUsersByStorageUsed({
-    fromPercent: 80,
-    toPercent: 85
-  })
-  if (usersFrom80to85.length) {
-    await emailService.sendUserEmails({
-      users: usersFrom80to85,
-      email: EMAIL_TYPE.User80PercentStorage
-    })
-  }
-
-  const usersFrom75to80 = await db.getUsersByStorageUsed({
-    fromPercent: 75,
-    toPercent: 80
-  })
-  if (usersFrom75to80.length) {
-    await emailService.sendUserEmails({
-      users: usersFrom75to80,
-      email: EMAIL_TYPE.User75PercentStorage
-    })
+      await emailService.sendUserEmails({
+        users,
+        email: email.emailType,
+        ...email.secondsSinceLastSent && { secondsSinceLastSent: email.secondsSinceLastSent }
+      })
+    }
   }
 
   log('✅ Done')
