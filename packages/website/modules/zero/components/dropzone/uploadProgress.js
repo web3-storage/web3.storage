ec005ffbe041f029340d0a40326104995ced14c9
Ben Munoz <benmunozj@gmail.com>
2022-04-20 21:42:49 +0200

    cleanup: Commented out code removed, console log statements removed

--- packages/website/modules/zero/components/dropzone/uploadProgress.js
+++ packages/website/modules/zero/components/dropzone/uploadProgress.js
@@ -4,31 +4,17 @@ import { useState, useEffect, useRef } from 'react';
 /**
  * @prop {string} [label]
  * @prop {number} [progress]
- * @prop {number} [rate]
  */
 
 const UploadProgress = ({ label, progress }) => {
   const [displayValue, setDisplayValue] = useState(0);
-  // const [running, setRunning] = useState(true);
   const lastChunkProgress = useRef(0);
   const lastChunkTime = useRef(0);
   const rate = useRef(0.1);
-  // const now = useRef(0);
-  // const then = useRef(0);
   const fps = 1;
   const fpsInterval = 1000 / fps;
 
   const incrementDisplayValue = () => {
-    // if (!running) {
-    //   return;
-    // }
-    // now.current = Date.now();
-    // const elapsed = now.current - then.current;
-    // if (elapsed > fpsInterval) {
-    //   then.current = now.current - (elapsed % fpsInterval);
-    //   setDisplayValue(displayValue => displayValue + rate.current);
-    // }
-    // window.requestAnimationFrame(incrementDisplayValue);
     setDisplayValue(displayValue => displayValue + rate.current);
   }
 
@@ -45,22 +31,13 @@ const UploadProgress = ({ label, progress }) => {
     if (lastChunkProgress.current === 0 || lastChunkTime.current === 0) {
       rate.current = 0.1;
     } else {
-      rate.current = Math.min(10, Math.max(0.1, ((progress - lastChunkProgress.current) / (Date.now() - lastChunkTime.current)) * 100));
+      rate.current = Math.min(3, Math.max(0.1, ((progress - lastChunkProgress.current) / (Date.now() - lastChunkTime.current)) * 100));
     }
-
     setDisplayValue(progress);
-    // then.current = Date.now();
-    // incrementDisplayValue();
-
     lastChunkProgress.current = progress;
     lastChunkTime.current = Date.now();
-
-    // return () => {
-    //   setRunning(false);
-    // };
   }, [progress]);
 
-  console.log(rate);
   return (
     <div className="loading-c">
       <span className="loading-label">{label}</span>
