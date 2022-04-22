cf596dbba09c5b8b0f34a6b279c224a50f2c18f4
Gary Homewood <gary@potatolondon.com>
2022-04-22 16:47:46 +0100

    chore: clearer test data setup

--- packages/cron/test/storage.spec.js
+++ packages/cron/test/storage.spec.js
@@ -11,6 +11,9 @@ import { getDBClient } from '../src/lib/utils.js'
 import { EMAIL_TYPE } from '@web3-storage/db'
 import { checkStorageUsed } from '../src/jobs/storage.js'
 import { EmailService } from '../src/lib/email.js'
+import { CID } from 'multiformats/cid'
+import { sha256 } from 'multiformats/hashes/sha2'
+import * as pb from '@ipld/dag-pb'
 import sinon from 'sinon'
 
 const env = {
@@ -26,19 +29,7 @@ const env = {
 
 describe('cron - check user storage quotas', () => {
   const dbClient = getDBClient(env)
-  let user1, user2, user3, user4, authKey1, authKey2, authKey3, authKey4
-  const cids = [
-    'bafybeiczsscdsbs7ffqz55asqdf3smv6klcw3gofszvwlyarci47fgf111',
-    'bafybeiczsscdsbs7ffqz55asqdf3smv6klcw3gofszvwlyarci47fgf112',
-    'bafybeiczsscdsbs7ffqz55asqdf3smv6klcw3gofszvwlyarci47fgf113'
-  ]
-  const cidsPinned = [
-    'QmdA5WkDNALetBn4iFeSepHjdLGJdxPBwZyY47ir1bZGAK',
-    'QmNvTjdqEPjZVWCvRWsFJA1vK7TTw1g9JP6we1WBJTRADM'
-  ]
-  const uploadSize = 439804651110
-  const pinnedSize = 109951162800
-  const largeFileSize = 3199023255550
+  let test2user, test3user, test4user
   const pins = [
     {
       status: 'Pinning',
@@ -58,79 +49,84 @@ describe('cron - check user storage quotas', () => {
     }
   ]
 
+  async function randomCid (code = pb.code) {
+    const hash = await sha256.digest(Buffer.from(`${Math.random()}`))
+    return CID.create(1, code, hash).toString()
+  }
+
+  /**
+   * Helper function to create a user with uploads and pin requests and a specified
+   * percentage of storage quota used
+   */
+  async function createUserWithFiles ({
+    email,
+    percentStorageUsed,
+    storageQuota = 1099511627776
+  }) {
+    const user = await createUser(dbClient, {
+      email,
+      name: email.replace('@email.com', '-name')
+    })
+
+    if (storageQuota !== 1099511627776) {
+      // non-default storage quota
+      await dbClient.createUserTag(Number(user._id), {
+        tag: 'StorageLimitBytes',
+        value: storageQuota.toString()
+      })
+    }
+
+    const authKey = await createUserAuthKey(dbClient, Number(user._id), {
+      name: `${email}-key`
+    })
+
+    const uploads = Math.floor(Math.random() * 5)
+    const pinRequests = Math.floor(Math.random() * 5)
+    const dagSize = Math.ceil(((percentStorageUsed / 100) * storageQuota) / (uploads + pinRequests))
+
+    for (let i = 0; i < uploads; i++) {
+      const cid = await randomCid()
+      await createUpload(dbClient, Number(user._id), Number(authKey), cid, {
+        dagSize
+      })
+    }
+
+    for (let i = 0; i < pinRequests; i++) {
+      const cid = await randomCid()
+      await createPsaPinRequest(dbClient, authKey, cid, {
+        dagSize,
+        pins
+      })
+    }
+
+    return user
+  }
+
   beforeEach(async () => {
     await createUser(dbClient, {
       email: 'admin@web3.storage',
       name: 'Web3 Storage Admin'
     })
 
-    user1 = await createUser(dbClient, {
-      name: 'test1-name',
-      email: 'test1@email.com'
-    })
-    authKey1 = await createUserAuthKey(dbClient, Number(user1._id), {
-      name: 'test1-key'
-    })
-    await createUpload(dbClient, Number(user1._id), Number(authKey1), cids[0], {
-      dagSize: uploadSize
-    })
-    await createPsaPinRequest(dbClient, authKey1, cidsPinned[0], {
-      dagSize: pinnedSize,
-      pins
-    })
-    await createPsaPinRequest(dbClient, authKey1, cidsPinned[1], {
-      dagSize: pinnedSize,
-      pins
+    await createUserWithFiles({
+      email: 'test1@email.com',
+      percentStorageUsed: 60
     })
 
-    user2 = await createUser(dbClient, {
-      name: 'test2-name',
-      email: 'test2@email.com'
-    })
-    authKey2 = await createUserAuthKey(dbClient, Number(user2._id), {
-      name: 'test2-key'
-    })
-    await createUpload(dbClient, Number(user2._id), Number(authKey2), cids[0], {
-      dagSize: uploadSize
-    })
-    await createUpload(dbClient, Number(user2._id), Number(authKey2), cids[1], {
-      dagSize: uploadSize
+    test2user = await createUserWithFiles({
+      email: 'test2@email.com',
+      percentStorageUsed: 79
     })
 
-    user3 = await createUser(dbClient, {
-      name: 'test3-name',
-      email: 'test3@email.com'
-    })
-    authKey3 = await createUserAuthKey(dbClient, Number(user3._id), {
-      name: 'test3-key'
-    })
-    await createUpload(dbClient, Number(user3._id), Number(authKey3), cids[0], {
-      dagSize: uploadSize
-    })
-    await createUpload(dbClient, Number(user3._id), Number(authKey3), cids[1], {
-      dagSize: Math.round(uploadSize * 1.2)
-    })
-    await createPsaPinRequest(dbClient, authKey3, cidsPinned[0], {
-      dagSize: pinnedSize,
-      pins
+    test3user = await createUserWithFiles({
+      email: 'test3@email.com',
+      percentStorageUsed: 90
     })
 
-    user4 = await createUser(dbClient, {
-      name: 'test4-name',
-      email: 'test4@email.com'
-    })
-    authKey4 = await createUserAuthKey(dbClient, Number(user4._id), {
-      name: 'test4-key'
-    })
-
-    await dbClient.createUserTag(Number(user4._id), {
-      tag: 'StorageLimitBytes',
-      value: '2199023255552'
-    })
-
-    const cid = 'bafybeibvuy3vcepqxy4plr34twv22vvxol2jjhmjxcrcvuhea5226whpsm'
-    await createUpload(dbClient, Number(user4._id), Number(authKey4), cid, {
-      dagSize: largeFileSize
+    test4user = await createUserWithFiles({
+      email: 'test4@email.com',
+      percentStorageUsed: 145,
+      storageQuota: 2199023255552
     })
   })
 
@@ -153,27 +149,28 @@ describe('cron - check user storage quotas', () => {
     })
     assert.strictEqual(adminStorageExceeded, true, 'Admin storage exceeded email sent')
 
-    const over100EmailSent = await dbClient.emailHasBeenSent({
-      userId: Number(user4._id),
-      emailType: EMAIL_TYPE.User100PercentStorage,
-      secondsSinceLastSent: 60 * 60 * 23
-    })
-    assert.strictEqual(over100EmailSent, true, 'Over 100% email sent')
-
-    const over90EmailSent = await dbClient.emailHasBeenSent({
-      userId: Number(user3._id),
-      emailType: EMAIL_TYPE.User90PercentStorage,
-      secondsSinceLastSent: 60 * 60 * 23
-    })
-    assert.strictEqual(over90EmailSent, true, 'Over 90% email sent')
-
     const over75EmailSent = await dbClient.emailHasBeenSent({
-      userId: Number(user2._id),
+      userId: Number(test2user._id),
       emailType: EMAIL_TYPE.User75PercentStorage,
       secondsSinceLastSent: 60 * 60 * 23
     })
     assert.strictEqual(over75EmailSent, true, 'Over 75% email sent')
 
+    const over90EmailSent = await dbClient.emailHasBeenSent({
+      userId: Number(test3user._id),
+      emailType: EMAIL_TYPE.User90PercentStorage,
+      secondsSinceLastSent: 60 * 60 * 23
+    })
+    assert.strictEqual(over90EmailSent, true, 'Over 90% email sent')
+
+    const over100EmailSent = await dbClient.emailHasBeenSent({
+      userId: Number(test4user._id),
+      emailType: EMAIL_TYPE.User100PercentStorage,
+      secondsSinceLastSent: 60 * 60 * 23
+    })
+    assert.strictEqual(over100EmailSent, true, 'Over 100% email sent')
+
+    // Ensure emails are not re-sent
     const { stderr: emailLog2 } = await execa('./src/bin/storage.js', { env })
     const log2Lines = emailLog2.split('\n')
     assert.match(log2Lines[0], /storage:checkStorageUsed 🗄 Checking users storage quotas/)
