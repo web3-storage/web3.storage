5a23e1d2827387ee084a2f3aca1786dba3be0629
Paolo <paolo@potatolondon.com>
2022-04-29 18:40:46 +0100

    chore: add dummy provider and move provider to own folder

--- packages/cron/src/bin/storage.js
+++ packages/cron/src/bin/storage.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 import { getDBClient } from '../lib/utils.js'
-import { EmailService } from '../lib/email/service.js'
+import { EmailService, EMAIL_PROVIDERS } from '../lib/email/service.js'
 import { checkStorageUsed } from '../jobs/storage.js'
 import { envConfig } from '../lib/env.js'
 
 async function main () {
   const db = getDBClient(process.env)
-  const emailService = new EmailService({ db })
+  const emailService = new EmailService({ db, provider: EMAIL_PROVIDERS.dummy })
   await checkStorageUsed({ db, emailService })
 }
 
