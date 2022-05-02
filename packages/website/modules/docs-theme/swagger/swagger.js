cba0de2a23b3e9173c68ff34243d4666a21c487a
Joanna Ong <ongjfu@gmail.com>
2022-05-02 07:28:02 -0400

    fix: swagger

--- packages/website/modules/docs-theme/swagger/swagger.js
+++ packages/website/modules/docs-theme/swagger/swagger.js
@@ -3,14 +3,12 @@ import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
 
 export default function Swagger() {
   useEffect(() => {
-    window.onload = function () {
-      const ui = SwaggerUIBundle({
-        url: '/schema.yml',
-        dom_id: '#swagger-ui',
-        presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-      });
-      window.ui = ui;
-    };
+    SwaggerUIBundle({
+      url: '/schema.yml',
+      dom_id: '#swagger-ui',
+      presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+      layout: 'StandaloneLayout',
+    });
   }, []);
   return <div id="swagger-ui"></div>;
 }
