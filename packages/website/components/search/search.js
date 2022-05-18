2dd88f492ca6c7f1adf8a07a675afd1668f29a4c
Joanna Ong <ongjfu@gmail.com>
2022-05-17 23:54:06 -0400

    fix: JS errors from docs changes (#1334)
    * fix: remove React fragment which is causing an error
    
    This was causing `Each child in a list should have a unique "key" prop`
    
    * fix: indentation from my previous commit
    
    * fix: js errors
    
    * fix: revert package-lock changes
    
    * fix: lint error
    
    Co-authored-by: Adam Alton <adamalton@gmail.com>

--- packages/website/components/search/search.js
+++ packages/website/components/search/search.js
@@ -1,5 +1,17 @@
 import { DocSearch } from '@docsearch/react';
+import { useState, useEffect } from 'react';
 
 export default function Search() {
-  return <DocSearch appId="9ARXAK1OFV" indexName="web3-storage-docusaurus" apiKey="358b95b4567a562349f2c806c152fada" />;
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <>
+      {mounted && (
+        <DocSearch appId="9ARXAK1OFV" indexName="web3-storage-docusaurus" apiKey="358b95b4567a562349f2c806c152fada" />
+      )}
+    </>
+  );
 }
