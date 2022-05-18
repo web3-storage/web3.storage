0ba92e24a642f67ced29d2d75f4767a8d96d87cc
Ben Munoz <benmunozj@gmail.com>
2022-05-18 11:33:47 -0400

    Revert "feat: Docs automatically generated files"
    This reverts commit 26ad9cadb30aa99439f08541494d57fef166533d.

--- packages/docs/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require('/Users/munoz/github/web3.storage/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/munoz/github/web3.storage/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/munoz/github/web3.storage/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/munoz/github/web3.storage/packages/docs/src/css/custom.scss'),
-  require('/Users/munoz/github/web3.storage/node_modules/docusaurus-theme-redoc/dist/custom.css'),
-  require('/Users/munoz/github/web3.storage/packages/docs/src/plugin/analytics'),
-];
