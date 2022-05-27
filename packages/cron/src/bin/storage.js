2bf1ef6994f65302686652594508d6b2845bd6c4
Paolo Chillari <flea89@users.noreply.github.com>
2022-05-27 11:28:05 +0100

    chore: use mailchimp provider in crons (#1368)

--- packages/cron/src/bin/storage.js
+++ packages/cron/src/bin/storage.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 import { getDBClient } from '../lib/utils.js'
-import { EmailService, EMAIL_PROVIDERS } from '../lib/email/service.js'
+import { EmailService } from '../lib/email/service.js'
 import { checkStorageUsed } from '../jobs/storage.js'
 import { envConfig } from '../lib/env.js'
 
 async function main () {
   const db = getDBClient(process.env)
-  const emailService = new EmailService({ db, provider: EMAIL_PROVIDERS.dummy })
+  const emailService = new EmailService({ db })
   await checkStorageUsed({ db, emailService })
 }
 
