5b6224da3631decf22eed93743a32c81b1e636ab
Paolo <paolo@potatolondon.com>
2022-06-15 10:18:41 +0100

    chore: update migration

--- packages/db/postgres/migrations/015-update-user-by-storage-used.sql
+++ packages/db/postgres/migrations/015-update-user-by-storage-used.sql
@@ -51,22 +51,3 @@ BEGIN
     ORDER BY user_account.storage_used::BIGINT DESC;
 END
 $$;
-
-CREATE OR REPLACE FUNCTION upsert_pins(data json) RETURNS TEXT[]
-    LANGUAGE plpgsql
-    volatile
-    PARALLEL UNSAFE
-AS
-$$
-DECLARE
-  pin json;
-  pin_ids TEXT[];
-BEGIN
-  FOREACH pin IN array json_arr_to_json_element_array(data -> 'pins')
-  LOOP
-    SELECT pin_ids || upsert_pin(pin -> 'data') INTO pin_ids;
-  END LOOP;
-
-  RETURN pin_ids;
-END
-$$;
