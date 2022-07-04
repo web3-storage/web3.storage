cc82c3a8e49e3ae454ce2cd85687a49c511d7ab3
Paolo <paolo@potatolondon.com>
2022-07-04 17:33:49 +0100

    feat: filter out Remote pins

--- packages/api/src/utils/pin.js
+++ packages/api/src/utils/pin.js
@@ -71,6 +71,9 @@ export async function getPins (cid, cluster, peerMap) {
 
   const pins = toPins(peerMap)
 
+  // Ignore Remote Pins
+  pins.filter(p => p.status !== 'Remote')
+
   if (!pins.length) {
     throw new Error('not pinning on any node')
   }
