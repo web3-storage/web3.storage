85994082778c3907368a8cbd386d4d7b47426db8
Joanna Ong <ongjfu@gmail.com>
2022-07-15 09:55:28 -0400

    feat: contact (#1637)
    * feat: contact page
    
    * feat: contact page
    
    * feat: update tweaks
    
    * fix: design feedback
    
    * fix: design feedback
    
    * fix: wording for contact and extra link in doc footer
    
    Co-authored-by: orun <orun@agencyundone.com>

--- packages/website/components/hero/hero.js
+++ packages/website/components/hero/hero.js
@@ -16,6 +16,7 @@ import ImageCorkscrew from '../../public/images/illustrations/corkscrew.png';
 import ImageRing from '../../public/images/illustrations/ring.png';
 import ImageSpring from '../../public/images/illustrations/spring.png';
 import ImageBlobs from '../../public/images/illustrations/blobs.png';
+import ImageCross2 from '../../public/images/illustrations/cross2.png';
 
 // ====================================================================== Params
 /**
@@ -92,6 +93,18 @@ export default function Hero({ block }) {
     );
   };
 
+  const contactHero = () => {
+    return (
+      <>
+        <HeroIllustration id="contact_hero-corkscrew" src={ImageCorkscrew} />
+        <HeroIllustration id="contact_hero-helix" src={ImageHelix} />
+        <HeroIllustration id="contact_hero-helix2" src={ImageHelix} />
+        <HeroIllustration id="contact_hero-cross" src={ImageCross2} />
+        <HeroIllustration id="contact_hero-triangle" src={ImageTriangle} />
+      </>
+    );
+  };
+
   const getPageHeroHeader = string => {
     switch (string) {
       case 'index':
@@ -104,6 +117,8 @@ export default function Hero({ block }) {
         return faqHero();
       case 'error':
         return hero404();
+      case 'contact':
+        return contactHero();
       default:
         return null;
     }
