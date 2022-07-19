7d1490746fc9d719dd2fb0c2a080a03368d223be
Vasco Santos <santos.vasco10@gmail.com>
2022-07-19 14:56:06 +0000

    fix: cron pins delete (#1653)

--- packages/cron/src/jobs/pins.js
+++ packages/cron/src/jobs/pins.js
@@ -12,7 +12,7 @@ import { downgradeCid } from '../lib/cid.js'
  * Bounded by URL length for deleting pin sync requests.
  * e.g. DELEETE /pin_sync_request?id=in.%28227623241%2C227623242%2C227623243%2C227623244%2C227623245...
  */
-const MAX_PIN_REQUESTS_PER_RUN = 700
+const MAX_PIN_REQUESTS_PER_RUN = 500
 /**
  * 8k max request length to cluster for statusAll, we hit this at around 126 CIDs
  * http://nginx.org/en/docs/http/ngx_http_core_module.html#large_client_header_buffers
