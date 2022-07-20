ccba73fd7e89bb2382ed4104f75c59f957cfb6b7
Vasco Santos <santos.vasco10@gmail.com>
2022-07-20 09:50:32 +0000

    chore: bring back cron to remove remote status pins (#1661)

---
+++ packages/cron/src/bin/delete-remote-pins.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import { deleteRemotePins } from '../jobs/delete-remote-pins.js'
+import { envConfig } from '../lib/env.js'
+import { getPgPool } from '../lib/utils.js'
+
+async function main () {
+  const rwPgPool = getPgPool(process.env, 'rw')
+  const batchSize = process.env.BATCH_SIZE ? parseInt(process.env.BATCH_SIZE, 10) : undefined
+
+  try {
+    deleteRemotePins({ rwPgPool, batchSize })
+  } finally {
+    await rwPgPool.end()
+  }
+}
+
+envConfig()
+main()
