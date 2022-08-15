81ee8b00f9aaec789050bf19275d3157f81fd7e5
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-08-15 12:36:38 -0700

    feat(@web3-storage/website): stub /account/payment page in website

---
+++ packages/website/pages/account/payment.js
@@ -0,0 +1,33 @@
+/**
+ * @fileoverview Account Payment Settings
+ */
+
+import AccountPageData from '../../content/pages/app/account.json';
+
+const PaymentSettingsPage = props => {
+  const { dashboard } = AccountPageData.page_content;
+  return (
+    <>
+      <>
+        <div className="page-container account-container">
+          <h1 className="table-heading">{dashboard.heading}</h1>
+          <div className="account-content">{props.title}</div>
+        </div>
+      </>
+    </>
+  );
+};
+
+/**
+ * @returns {{ props: import('components/types').PageProps}}
+ */
+export function getStaticProps() {
+  return {
+    props: {
+      title: AccountPageData.seo.title,
+      isRestricted: true,
+    },
+  };
+}
+
+export default PaymentSettingsPage;
