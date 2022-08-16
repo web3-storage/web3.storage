ff5f586ab56480a7c6b5a0be7078e558d516f827
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-08-16 14:37:40 -0700

    wip

--- packages/website/tests/playwright/globalSetup.ts
+++ packages/website/tests/playwright/globalSetup.ts
@@ -2,8 +2,15 @@ import { spawn } from 'child_process';
 import * as path from 'path';
 
 export default async function main() {
+  console.log('globalSetup', {
+    env: {
+      MAGIC_SECRET_KEY_typeOf: typeof process.env.MAGIC_SECRET_KEY,
+      MAGIC_SECRET_KEY_length: String(process.env.MAGIC_SECRET_KEY).length,
+    },
+  });
   const w3storageProcess = spawn('npm', ['start'], {
     cwd: path.join(__dirname, '../../../../'),
+    shell: true,
   });
   w3storageProcess.stdout.on('data', function (msg) {
     console.log('[stdout]:', msg.toString());
