afb92f42a30e76316644cc9cc8fd88c18a023d4b
Johnathon Roach <johnathon.roach@gmail.com>
2022-08-17 15:00:40 -0500

    feat: make list tag configurable

--- packages/api/src/utils/mailchimp.js
+++ packages/api/src/utils/mailchimp.js
@@ -24,11 +24,12 @@ export const isChimpUser = async (email, prefix, listId, headers) => {
  * @returns {Promise<Response>}
  */
 export const addSubscriber = async (email, prefix, listId, headers) => {
+  const tag = process.env.MAILCHIMP_API_LIST_TAG || 'web3_blog_subscriber';
   const url = `https://${prefix}.api.mailchimp.com/3.0/lists/${listId}/members/`
   const body = JSON.stringify({
     email_address: email,
     status: 'subscribed',
-    tags: ['web3_blog_subscriber']
+    tags: [tag]
   })
 
   return await fetch(url, {
@@ -46,11 +47,12 @@ export const addSubscriber = async (email, prefix, listId, headers) => {
  * @returns {Promise<Response>}
  */
 export const updateSubscriber = async (email, prefix, listId, headers) => {
+  const tag = process.env.MAILCHIMP_API_LIST_TAG || 'web3_blog_subscriber';
   const url = `https://${prefix}.api.mailchimp.com/3.0/lists/${listId}/members/${encodeURIComponent(
     email
   )}/tags`
   const body = JSON.stringify({
-    tags: [{ name: 'web3_blog_subscriber', status: 'active' }]
+    tags: [{ name: tag, status: 'active' }]
   })
 
   return await fetch(url, {
