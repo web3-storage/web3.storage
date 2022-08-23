4389ba0dad3441ca86d0a0c9e01cee9583be74ca
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-08-23 15:34:47 -0700

    add comment to envfile script explaining ENVFILE_WITHOUT
    Co-authored-by: Yusef Napora <yusef@napora.org>

--- packages/website/scripts/envfile.ts
+++ packages/website/scripts/envfile.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 
 /**
  * print a good .env file to stdout that can be used for web3.storage
+ * The `ENVFILE_WITHOUT` env variable may contain a comma-separated list of variable names to exclude from the output.
  */
 export async function main() {
   const without = (process.env.ENVFILE_WITHOUT ?? '').split(',')
