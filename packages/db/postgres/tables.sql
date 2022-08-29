59255ff6974271cbaf643004d96a055aec137114
Ericsson de Oliveira <cssonddos@gmail.com>
2022-08-29 17:55:16 +0100

    fix: add source_cid index on upload table (#1627)
    * fix: add source_cid index on upload table
    
    * added index to tables file
    
    * removed concurrently keyword for index
    
    * applied standard name convention

--- packages/db/postgres/tables.sql
+++ packages/db/postgres/tables.sql
@@ -269,6 +269,7 @@ CREATE INDEX IF NOT EXISTS upload_auth_key_id_idx ON upload (auth_key_id);
 CREATE INDEX IF NOT EXISTS upload_user_id_deleted_at_idx ON upload (user_id) WHERE deleted_at IS NULL;
 CREATE INDEX IF NOT EXISTS upload_content_cid_idx ON upload (content_cid);
 CREATE INDEX IF NOT EXISTS upload_updated_at_idx ON upload (updated_at);
+CREATE INDEX IF NOT EXISTS upload_source_cid_idx ON upload (source_cid);
 
 -- A request to keep a Pin in sync with the nodes that are pinning it.
 CREATE TABLE IF NOT EXISTS pin_sync_request
