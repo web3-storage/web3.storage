99654bcc668db016710a91e07901cd40b9a9995a
Josh Jarvis <josh.jarvis@potatolondon.com>
2022-09-08 15:38:33 +0100

    fix: add a `/user/uploads/:cid` endpoint. (#1530)
    * fix: Add a `/user/uploads/:cid` endpoint for returning a single upload by cid.
    
    * fix: Fix tests and return 404.
    
    * chore: Update documentation to include get single upload.
    
    * chore: ignore failing linting lines similarly to other endpoints.

--- packages/api/test/user.spec.js
+++ packages/api/test/user.spec.js
@@ -308,6 +308,29 @@ describe('GET /user/uploads', () => {
   })
 })
 
+describe('GET /user/upload/:cid', () => {
+  it('gets a single upload', async () => {
+    const token = await getTestJWT()
+    const res = await fetch(new URL(`/user/uploads/${userUploads[0].cid}`, endpoint).toString(), {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    assert(res.ok)
+    const uploads = await res.json()
+    assert.deepStrictEqual(uploads, userUploads[0])
+  })
+
+  it('returns 404 when no upload is found', async () => {
+    const token = await getTestJWT()
+    const res = await fetch(new URL('/user/uploads/notfound', endpoint).toString(), {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    assert(!res.ok)
+    assert.strictEqual(res.status, 404)
+  })
+})
+
 describe('DELETE /user/uploads/:cid', () => {
   it('error if not authenticated with magic.link', async () => {
     const token = await getTestJWT()
