e4e22a00de44f37849279bf2da7d55ec648340ef
Josh Ghent <me@joshghent.com>
2022-09-09 11:23:09 +0100

    fix: updated test mocks

--- packages/cron/test/pins-backup.spec.js
+++ packages/cron/test/pins-backup.spec.js
@@ -1,27 +1,23 @@
 /* eslint-env mocha */
-import { createPsaPinRequest, createUser, createUserAuthKey } from '@web3-storage/db/test-utils'
-import { S3Client, ListObjectsV2Command, GetObjectCommand } from '@aws-sdk/client-s3'
-import * as backup from '../src/jobs/pins-backup.js'
-import { getCluster, getDBClient } from '../src/lib/utils.js'
+import { createPsaPinRequest, createUser, createUpload, createUserAuthKey, randomCid } from '@web3-storage/db/test-utils'
+import Backup from '../src/jobs/pins-backup.js'
+import assert from 'assert'
+import { getCluster, getDBClient, getPg } from '../src/lib/utils.js'
 import sinon from 'sinon'
 import fetch from '@web-std/fetch'
-
-const S3_BUCKET_ENDPOINT = 'http://127.0.0.1:9000'
-const S3_BUCKET_NAME = 'dotstorage-test-0'
-const S3_BUCKET_REGION = 'us-east-1'
-const S3_ACCESS_KEY_ID = 'minioadmin'
-const S3_SECRET_ACCESS_KEY_ID = 'minioadmin'
 
 global.fetch = fetch
 
 const env = {
-  DEBUG: '*',
+  DEBUG: 'backup:pins',
   ENV: 'dev',
   CLUSTER_API_URL: 'http://localhost:9094',
   CLUSTER_IPFS_PROXY_API_URL: 'http://127.0.0.1:9095/api/v0/',
   CLUSTER_BASIC_AUTH_TOKEN: 'dGVzdDp0ZXN0',
   DATABASE: 'postgres',
   PG_REST_URL: 'http://localhost:3000',
+  RO_PG_CONNECTION: 'postgres://postgres:postgres@127.0.0.1:5432/postgres',
+  PG_CONNECTION: 'postgres://postgres:postgres@127.0.0.1:5432/postgres',
   PG_REST_JWT: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJyb2xlIjoic2VydmljZV9yb2xlIn0.necIJaiP7X2T2QjGeV-FhpkizcNTX8HjDDBAxpgQTEI'
 }
 
@@ -31,7 +27,7 @@ const ipfsPeerId = '1'
 const region = '1'
 
 const uploadPins = [{
-  status: 'Pinning',
+  status: 'Pinned',
   location: {
     peerId,
     peerName,
@@ -67,46 +63,113 @@ const createPin = (cid, status = 'pinned') => {
 }
 
 describe('cron - pins backup', () => {
-  let user, userId, authKey, dbClient, cluster, s3
-  const cids = ['bafy1', 'bafy3', 'bafy4']
+  let user, userId, authKey, dbClient, cluster, s3, backup, rwPg, roPg
+  const cids = []
 
   beforeEach(async () => {
     dbClient = getDBClient(env)
     cluster = getCluster(env)
+    roPg = await getPg(env, 'ro')
+    rwPg = await getPg(env, 'rw')
+    backup = new Backup(env)
 
     user = await createUser(dbClient)
     userId = parseInt(user._id)
+    console.log(userId)
     authKey = parseInt(await createUserAuthKey(dbClient, userId))
 
+    cids[0] = await randomCid()
+    cids[1] = await randomCid()
+    cids[2] = await randomCid()
+
+    console.log(cids)
+
     // Create 3 uploads with status Pinned
+    await createUpload(dbClient, userId, authKey, cids[0], { pins: uploadPins })
+    await createUpload(dbClient, userId, authKey, cids[1], { pins: uploadPins })
+    await createUpload(dbClient, userId, authKey, cids[2], { pins: uploadPins })
     await createPsaPinRequest(dbClient, authKey, cids[0], { pins: uploadPins })
     await createPsaPinRequest(dbClient, authKey, cids[1], { pins: uploadPins })
     await createPsaPinRequest(dbClient, authKey, cids[2], { pins: uploadPins })
 
-    dbClient.query('UPDATE pin SET (status) VALUES (\'Pinned\') WHERE content_cid IN ($1, $2, $3)', cids)
+    // const exportCar = (cidNumber) => {
+    //   return async function * () {
+    //     console.log('mocking response of IPFS return')
+    //     yield {
+    //       content: Buffer.from('test file'),
+    //       contentCid: cids[cidNumber],
+    //       sourceCid: cids[cidNumber]
+    //     }
+    //   }
+    // }
 
-    // TODO: Mock the S3 client
-    s3 = new S3Client({
-      endpoint: S3_BUCKET_ENDPOINT,
-      forcePathStyle: true,
-      region: S3_BUCKET_REGION,
-      credentials: {
-        accessKeyId: S3_ACCESS_KEY_ID,
-        secretAccessKey: S3_SECRET_ACCESS_KEY_ID
+    const exportCarStub = sinon.stub(backup, 'exportCar')
+
+    exportCarStub.onCall(0).returns(async function * () {
+      console.log('mocking response of IPFS return')
+      yield {
+        content: Buffer.from('test file'),
+        contentCid: cids[0],
+        sourceCid: cids[0]
+      }
+    })
+    exportCarStub.onCall(1).returns(async function * () {
+      console.log('mocking response of IPFS return')
+      yield {
+        content: Buffer.from('test file'),
+        contentCid: cids[1],
+        sourceCid: cids[1]
+      }
+    })
+    exportCarStub.onCall(2).returns(async function * () {
+      console.log('mocking response of IPFS return')
+      yield {
+        content: Buffer.from('test file'),
+        contentCid: cids[2],
+        sourceCid: cids[2]
       }
     })
 
-    // TODO: Mock the cluster response for getting the car file
-    // const { root, car: carBody } = await createCar('hello world! s3')
+    // const uploadCar = (cidNumber) => {
+    //   return async function * () {
+    //     console.log('mocking upload of car file')
+    //     yield {
+    //       content: Buffer.from('test file'),
+    //       contentCid: cids[cidNumber],
+    //       sourceCid: cids[cidNumber],
+    //       backupUrl: 'https://test.s3.eu-west-1.amazonaws.com/my-car'
+    //     }
+    //   }
+    // }
 
-    sinon.stub(backup, 'exportCar').returns({
-      content: Buffer.from({}),
-      contentCid: cids[0],
-      sourceCid: cids[0]
+    const uploadCarStub = sinon.stub(backup, 'uploadCar')
+
+    uploadCarStub.onCall(0).returns(async function * () {
+      console.log('mocking upload of car file')
+      yield {
+        content: Buffer.from('test file'),
+        contentCid: cids[0],
+        sourceCid: cids[0],
+        backupUrl: `https://test.s3.eu-west-1.amazonaws.com/my-car/${cids[0]}`
+      }
     })
-
-    sinon.stub(backup, 'uploadCar').returns({
-      backupUrl: 'https://test.s3.eu-west-1.amazonaws.com/my-car'
+    uploadCarStub.onCall(1).returns(async function * () {
+      console.log('mocking upload of car file')
+      yield {
+        content: Buffer.from('test file'),
+        contentCid: cids[1],
+        sourceCid: cids[1],
+        backupUrl: `https://test.s3.eu-west-1.amazonaws.com/my-car/${cids[1]}`
+      }
+    })
+    uploadCarStub.onCall(2).returns(async function * () {
+      console.log('mocking upload of car file')
+      yield {
+        content: Buffer.from('test file'),
+        contentCid: cids[2],
+        sourceCid: cids[2],
+        backupUrl: `https://test.s3.eu-west-1.amazonaws.com/my-car/${cids[2]}`
+      }
     })
 
     // TODO: Mock cluster resposne
@@ -121,15 +184,15 @@ describe('cron - pins backup', () => {
     }
   })
 
+  after(async () => {
+    await roPg.end()
+    await rwPg.end()
+  })
+
   it.only('should attempt to backup pins to S3', async () => {
-    await backup.backupPins(env, dbClient, dbClient, cluster)
-
-    // TODO: Verify bucket has file in
-    const listRes = await s3.send(new ListObjectsV2Command({
-      Bucket: S3_BUCKET_NAME
-    }))
-    console.log(listRes)
-
-    // TODO: Verify that the psa_pin_request has a backup_url
+    await backup.backupPins({ env, roPg, rwPg, cluster })
+    const res = await roPg.query('SELECT * FROM psa_pin_request')
+    console.log(res.rows)
+    assert.strictEqual(res.rows.every(row => row.backup_urls), true, 'Not all pin requests have a backup!')
   })
 })
