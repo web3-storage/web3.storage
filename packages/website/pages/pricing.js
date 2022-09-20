85dee70dbadb7e34762ebfccfd37862a2e6d5ec0
Drew <22156330+drewdelano@users.noreply.github.com>
2022-09-20 15:27:36 -0700

    fix: more blob and cta styling

--- packages/website/pages/pricing.js
+++ packages/website/pages/pricing.js
@@ -171,6 +171,7 @@ export default function Home() {
                         company and what you&apos;re building using the form below, and we&apos;ll get back to you
                         shortly.
                       </div>
+                      <div className="enterprise-above-call-to-action"></div>
                       <div>
                         <div className="enterprise-call-to-action">LET&apos;S CHAT</div>
                       </div>
