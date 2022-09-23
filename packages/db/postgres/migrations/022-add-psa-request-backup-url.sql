36e7e081b5286109d2d4c0515c9668ca00e2e699
Josh Ghent <me@joshghent.com>
2022-09-23 18:51:05 +0100

    feat: added default to empty array

--- packages/db/postgres/migrations/022-add-psa-request-backup-url.sql
+++ packages/db/postgres/migrations/022-add-psa-request-backup-url.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "psa_pin_request" ADD COLUMN IF NOT EXISTS "backup_urls" TEXT[];
+ALTER TABLE "psa_pin_request" ADD COLUMN IF NOT EXISTS "backup_urls" TEXT[] DEFAULT array[]::text[];
 CREATE INDEX CONCURRENTLY IF NOT EXISTS psa_pin_request_backup_urls_idx ON psa_pin_request (backup_urls);
