3efe1d3c92e8f418e4d5a3b3aa37000a5ecaf26a
Drew <22156330+drewdelano@users.noreply.github.com>
2022-10-10 17:01:20 -0700

    fix: swap redirectTo for requiresAuth

--- packages/website/pages/account/payment.js
+++ packages/website/pages/account/payment.js
@@ -219,7 +219,7 @@ export function getStaticProps() {
     props: {
       title: 'Payment',
       isRestricted: true,
-      redirectTo: '/login/?redirect_uri=/account/payment',
+      requiresAuth: true,
       stripePublishableKey,
       breadcrumbs: [crumbs.index, crumbs.payment],
     },
