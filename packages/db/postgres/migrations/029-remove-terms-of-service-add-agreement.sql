51dd747d67d4cde5702eaf56d220d1957c34b9bc
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-10-20 17:06:44 +0100

    adjust migration 29 to use RENAME instead of drop/create

--- packages/db/postgres/migrations/029-remove-terms-of-service-add-agreement.sql
+++ packages/db/postgres/migrations/029-remove-terms-of-service-add-agreement.sql
@@ -1,12 +1,2 @@
-DROP TABLE IF EXISTS terms_of_service;
-DROP INDEX IF EXISTS terms_of_service_user_id_idx;
-
-CREATE TABLE IF NOT EXISTS agreement (
-  id              BIGSERIAL PRIMARY KEY,
-  user_id         BIGINT                                                        NOT NULL REFERENCES public.user (id),
-  agreement_type  agreement_type                                                NOT NULL,
-  inserted_at     TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL,
-  UNIQUE (user_id, agreement_type)
-);
-
-CREATE INDEX IF NOT EXISTS agreement_user_id_idx ON agreement (user_id);
+ALTER TABLE terms_of_service RENAME TO agreement; 
+ALTER INDEX terms_of_service_user_id_idx RENAME TO agreement_user_id_idx;
