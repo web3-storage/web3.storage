77564ff0cb6d113f5952374baca9130086c18360
Adam Alton <270255+adamalton@users.noreply.github.com>
2022-12-12 13:31:21 +0000

    fix: wait for the dev/api servers to be ready before running e2e tests

--- packages/website/tests/playwright/globalSetup.ts
+++ packages/website/tests/playwright/globalSetup.ts
@@ -1,11 +1,21 @@
 import { spawn } from 'child_process';
+import fetch from 'node-fetch';
 import * as path from 'path';
 
-export default async function main() {
+export default async function main(config) {
   console.log('Starting w3 storage process for tests...');
   const w3storageProcess = spawn('npm', ['start'], {
     cwd: path.join(__dirname, '../../../../'),
   });
+
+  // Wait for the dev server to get up and running before attempting to start the tests,
+  // otherwise the first test will fail just trying to connect.
+  const { baseURL } = config.projects[0].use;
+  const apiURL = process.env.NEXT_PUBLIC_API;
+
+  await waitForServer(baseURL, 'web');
+  await waitForServer(apiURL, 'API');
+
   for (const fd of ['stdout', 'stderr']) {
     const prefix = process.env.PLAYWRIGHT_PREFIX_FD ? `[${fd}]: ` : '';
     w3storageProcess[fd].on('data', log => {
@@ -19,9 +29,26 @@ export default async function main() {
 }
 
 if (process.env.RUN) {
-  main();
+  console.log('What is this for?')
+  // main();
 }
 
 function prepender(prefix) {
   return suffix => `${prefix}${suffix}`;
+}
+
+async function waitForServer (url, name) {
+  let connected = false;
+  const maxAttempts = 5;
+  let attempt = 0;
+  while (attempt < maxAttempts && !connected) {
+    attempt++;
+    try {
+      console.log('Seeing if the dev server is ready...')
+      const response = await fetch(url);
+      connected = response.ok
+    } catch (error) {
+      console.log(`Failed to connect to ${name} server (attempt ${attempt} of ${maxAttempts}`)
+    }
+  }
 }
