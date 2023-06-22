a2df7b6b505a280c893e6934ca41c1186e78f49f
Alan Shaw <alan.shaw@protocol.ai>
2023-06-22 12:19:20 +0100

    fix: CAR CID decoding from path

--- packages/api/src/utils/bucket.js
+++ packages/api/src/utils/bucket.js
@@ -1,18 +1,17 @@
 import { fromString } from 'uint8arrays'
 import * as Digest from 'multiformats/hashes/digest'
-import { sha256 } from 'multiformats/hashes/sha2'
 import { CID } from 'multiformats/cid'
 import { CAR_CODE } from '../constants.js'
 
 /**
- * Converts a bucket path in format `raw/<root>/<user>/<base64(car-digest)>.car`
+ * Converts a bucket path in format `raw/<root>/<user>/<base32(car-multihash)>.car`
  * to a CAR CID.
  * @param {string} path
  */
 export function rawCarPathToShardCid (path) {
   const parts = String(path).split('/')
   const filename = String(parts[3])
-  const digestBytes = fromString(filename.split('.')[0], 'base64pad')
-  const digest = Digest.create(sha256.code, digestBytes)
+  const digestBytes = fromString(filename.split('.')[0], 'base32')
+  const digest = Digest.decode(digestBytes)
   return CID.createV1(CAR_CODE, digest)
 }
