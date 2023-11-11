370ed08aefa7d98c75396c6c03376b095cf20799
bengo <171782+gobengo@users.noreply.github.com>
2023-11-10 16:06:40 -0800

    move W3upMigrationRecommendationCopy from w3up-launch package to module in website to avoid headaches with jsx in api mocha

--- packages/w3up-launch/src/index.js
+++ packages/w3up-launch/src/index.js
@@ -92,23 +92,3 @@ export const createW3upLaunchConfig = (launch) => {
     shouldBlockNewUserSignupsBecauseProductSunset: shouldBlockNewUserSignupsBecauseProductSunset(launch)
   }
 }
-
-/**
- * copy for banner message across top of some web3.storage pages when w3up ships
- * @param {object} props
- * @param {Date} props.sunsetStartDate
- */
-export const W3upMigrationRecommendationCopy = ({ sunsetStartDate }) => {
-  const createNewAccountHref = 'https://console.web3.storage/?intent=create-account'
-  const learnWhatsNewHref = 'https://console.web3.storage/?intent=learn-new-web3storage-experience'
-  const sunsetDateFormatter = new Intl.DateTimeFormat(undefined, { dateStyle: 'long' })
-  return (
-    <>
-      This web3.storage product will sunset on {sunsetDateFormatter.format(sunsetStartDate)}. We recommend migrating
-      your usage of web3.storage to the new web3.storage.
-      <br />
-      <a href={createNewAccountHref}>Click here to create a new account</a> and&nbsp;
-      <a href={learnWhatsNewHref}>here to read about what’s awesome</a> about the new web3.storage experience.
-    </>
-  )
-}
--- packages/website/components/w3up-launch.js
+++ packages/website/components/w3up-launch.js
@@ -7,3 +7,23 @@ export * from '@web3-storage/w3up-launch';
 export function useW3upLaunch(context = W3upLaunchContext) {
   return React.useContext(context);
 }
+
+/**
+ * copy for banner message across top of some web3.storage pages when w3up ships
+ * @param {object} props
+ * @param {Date} props.sunsetStartDate
+ */
+export const W3upMigrationRecommendationCopy = ({ sunsetStartDate }) => {
+  const createNewAccountHref = 'https://console.web3.storage/?intent=create-account';
+  const learnWhatsNewHref = 'https://console.web3.storage/?intent=learn-new-web3storage-experience';
+  const sunsetDateFormatter = new Intl.DateTimeFormat(undefined, { dateStyle: 'long' });
+  return (
+    <>
+      This web3.storage product will sunset on {sunsetDateFormatter.format(sunsetStartDate)}. We recommend migrating
+      your usage of web3.storage to the new web3.storage.
+      <br />
+      <a href={createNewAccountHref}>Click here to create a new account</a> and&nbsp;
+      <a href={learnWhatsNewHref}>here to read about what’s awesome</a> about the new web3.storage experience.
+    </>
+  );
+};
